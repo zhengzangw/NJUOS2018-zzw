cb9ddce9596e1634b36ab728ab8016ae4b56e446
tracer-nju <tracer@nju.edu.cn>
2019-02-27 18:52:55 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: 1d94a31def7a466db140eb21483f097e     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  18:52:55 up 11:23,  4 users,  load average: 0.15, 0.22, 0.51

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -81,7 +81,7 @@ void getinfo(struct Process *ret, int pid)
                 assert(0);
 }
 
-void search(struct Process *cur, int depth, char pre[512], int prelen)
+void search(struct Process *cur, int depth, char pre[128], int prelen)
 {
         printf("-%s%s(%d)-\n", pre, cur->name, cur->pid);
         if (cur->nson+cur->nthr>1) printf("+-"); else printf("--");
