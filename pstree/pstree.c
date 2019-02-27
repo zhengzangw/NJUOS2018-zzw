9441025e61fa1a21d3f30eb2127f6026638759d3
tracer-nju <tracer@nju.edu.cn>
2019-02-27 12:03:40 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: 1d94a31def7a466db140eb21483f097e     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  12:03:40 up  6:26,  4 users,  load average: 0.68, 0.63, 0.39

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -15,78 +15,80 @@ bool visit[32768];
 
 bool isnumber(char *s, int length)
 {
-	for (int i = 0; i < length; ++i) {
-		if (!(s[i] >= '0' && s[i] <= '9')) {
-			return false;
-		}
-	}
-	return true;
+        for (int i = 0; i < length; ++i) {
+                if (!(s[i] >= '0' && s[i] <= '9')) {
+                        return false;
+                }
+        }
+        return true;
 }
 
 void search(int cur, int depth)
 {
-	if (!visit[cur]) {
-		visit[cur] = 1;
+        if (!visit[cur]) {
+                visit[cur] = 1;
 
-		sprintf(filename, "/proc/%d/comm", cur);
-		FILE *fp = fopen(filename, "r");
-		if (fp) {
-			fscanf(fp, "%s", procname);
-			fclose(fp);
-		} else {
-			printf("Error on %s\n", filename);
-		}
+                sprintf(filename, "/proc/%d/comm", cur);
+                FILE *fp = fopen(filename, "r");
+                if (fp) {
+                        fscanf(fp, "%s", procname);
+                        fclose(fp);
+                } else {
+                        printf("Error on %s\n", filename);
+                }
 
-		printf("%*s%s(%d)\n", depth, "", procname, cur);
+                printf("%*s%s(%d)\n", depth, "", procname, cur);
 
-		int ch;
-		sprintf(filename, "/proc/%d/task/%d/children", cur, cur);
-		fp = fopen(filename, "r");
-		if (fp) {
-			fscanf(fp, "%d", &ch);
-			search(ch, depth + 1);
-			fclose(fp);
-		} else {
-			printf("Error on %s\n", filename);
-		}
-	}
+                int ch;
+                sprintf(filename, "/proc/%d/task/%d/children", cur, cur);
+                fp = fopen(filename, "r");
+                if (fp) {
+                        while (fscanf(fp, "%d", &ch) != EOF) {
+                                search(chndentLine_enabled = 1, depth + 1);
+                        }
+                        fclose(fp);
+                } else {
+                        printf("Error on %s\n", filename);
+                }
+        }
 }
 
 int main(int argc, char *argv[])
 {
-	// getopt
-	bool issort = false, showpid = false;
-	int opt;
-	while ((opt = getopt(argc, argv, "Vnp")) != -1) {
-		switch (opt) {
-		case 'V':
-			printf("pstree 1.0\nCopyright (C) 2019 Zheng Zangwei\n");
-			return 0;
-		case 'n':
-			issort = true;
-			break;
-		case 'p':
-			showpid = true;
-		default:
-			printf("Only -V,-n,-p is available.\n");
-			return -1;
-		}
-	}
-	printf("%d%d\n", issort, showpid);
+        // getopt
+        bool issort = false, showpid = false;
+        int opt;
+        while ((opt = getopt(argc, argv, "Vnp")) != -1) {
+                switch (opt) {
+                case 'V':
+                        printf
+                            ("pstree 1.0\nCopyright (C) 2019 Zheng Zangwei\n");
+                        return 0;
+                case 'n':
+                        issort = true;
+                        break;
+                case 'p':
+                        showpid = true;
+                default:
+                        printf("Only -V,-n,-p is available.\n");
+                        return -1;
+                }
+        }
+        printf("%d%d\n", issort, showpid);
 
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir("/proc/")) != NULL) {
-		while ((ent = readdir(dir)) != NULL) {
-			if (isnumber(ent->d_name, strlen(ent->d_name))) {
-				int cur = atoi(ent->d_name);
-				search(cur, 0);
-			}
-		}
-		closedir(dir);
-	} else {
-		perror("");
-		return -1;
-	}
-	return 0;
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir("/proc/")) != NULL) {
+                while ((ent = readdir(dir)) != NULL) {
+                        if (isnumber(ent->d_name, strlen(ent->d_name))) {
+                                int cur = atoi(ent->d_name);
+                                search(cur, 0);
+                        }
+                }
+                closedir(dir);
+        } else {
+                perror("");
+                return -1;
+        }
+        return 0;
 }
