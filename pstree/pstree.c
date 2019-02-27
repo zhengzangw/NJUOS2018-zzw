0af2b4316fee6f81d29487c2acd7f07c6b5319cd
tracer-nju <tracer@nju.edu.cn>
2019-02-27 10:03:12 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: 1d94a31def7a466db140eb21483f097e     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  10:03:12 up  4:25,  4 users,  load average: 0.29, 0.13, 0.08

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
             issort = true;
             break;
           case 'p':
-            showpid = true
+            showpid = true;
       }
   }
   return 0;
