5ef47038021bfc5995e8e86740de85f0a9d3ab47
tracer-nju <tracer@nju.edu.cn>
2019-02-27 21:10:45 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: 1d94a31def7a466db140eb21483f097e     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  21:10:45 up 13:41,  4 users,  load average: 0.77, 0.68, 0.60

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -103,20 +103,29 @@ bool isroot = true;
 void search(struct Process *cur, int type, bool isproc)
 {
         if (isroot) {
+            if (showpid)
                 sprintf(tmp, "%s(%d)", cur->name, cur->pid);
+            else
+                sprintf(tmp, "%s", cur->name);
                 isroot = false;
                 stack[++head] = strlen(tmp) + 1;
         } else {
                 switch (type) {
                 case 0:        //first of one subtree
-                        sprintf(tmp, "-%s(%d)", cur->name, cur->pid);
+            if (showpid)
+                sprintf(tmp, "%s(%d)", cur->name, cur->pid);
+            else
+                sprintf(tmp, "%s", cur->name);
                         stack[head + 1] = stack[head] + strlen(tmp) + 1;
                         head++;
                         break;
                 case -1:
                         pre[stack[head] - 1] = '\\';
                 default:
+                if (showpid)
                         sprintf(tmp, "%s-%s(%d)", pre, cur->name, cur->pid);
+                else
+                    sprintf(tmp, "%s-%s", pre, cur->name);
                         stack[++head] = strlen(tmp) + 1;
                 }
         }
