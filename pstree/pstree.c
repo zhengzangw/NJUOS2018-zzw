43347a57ce892c8e8df309f93f878959f9191f05
tracer-nju <tracer@nju.edu.cn>
2019-02-27 16:18:56 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: 1d94a31def7a466db140eb21483f097e     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  16:18:56 up  9:18,  4 users,  load average: 0.37, 0.28, 0.18

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -113,7 +113,7 @@ void getinfo(struct Process * ret, int pid){
           int tid = atoi(ent->d_name);
           if (tid != pid) {
               ret->son[ret->nson] = malloc(sizeof(struct Process));
-          //    strcpy(ret->son[ret->nson]->name,(strcat("{", ret->name),"}"));
+              strcpy(ret->son[ret->nson]->name,(strcat("{", ret->name),"}"));
           //    ret->son[ret->nson]->pid = tid;
           //    ret->son[ret->nson]->isproc = false;
               ret->nson ++;
