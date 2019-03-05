d2a4556e1abe051860bc7dd993e91528856b3802
tracer-nju <tracer@nju.edu.cn>
2019-03-05 11:20:50 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: bdd06ff6d9eb41ce8dbeaa6633ede5c5     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  11:20:50 up  1:25,  4 users,  load average: 1.22, 1.80, 1.33

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -2,24 +2,38 @@
 #include <klib.h>
 
 #define ISKEYDOWN(x) (((x)&0x8000))
+#define KEYCODE(x) ((x)&0x7fff)
 const int FPS = 1;
 static int weight, height, next_frame, key;
 
 void init_screen();
-void splash();
+void splash(int);
+
+int t = 0;
+
+void game_progress(){
+}
+
+void screen_update(){
+  t ^= 1;
+  splash(t);
+}
 
 int main() {
   // Operating system is a C program
   _ioe_init();
   init_screen();
 
-  splash();
   while (1) {
     while (uptime() < next_frame);
-    printf("%d\n", _KEY_W);
     while ((key = read_key())!=_KEY_NONE && ISKEYDOWN(key)){
-          printf("%d\n", key);
+        switch KEYCODE(key){
+          case _KEY_W: printf("Hello\n"); break;
+          default: break;
+        }
     }
+    game_progress();
+    screen_update();
     next_frame += 1000/FPS;
   }
 
@@ -45,10 +59,10 @@ void draw_srect(int x, int y, int w, int h, uint32_t color) {
   _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &event, sizeof(event));
 }
 
-void splash() {
+void splash(int t) {
   for (int x = 0; x * SIDE <= weight; x ++) {
     for (int y = 0; y * SIDE <= height; y++) {
-      if ((x & 1) ^ (y & 1)) {
+      if (((x & 1) ^ (y & 1))==t) {
         draw_srect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffff00); // white
       }
     }
