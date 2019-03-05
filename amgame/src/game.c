068c38988315b94ad01bb5d2ed4a3a835dde5cdc
tracer-nju <tracer@nju.edu.cn>
2019-03-05 21:23:35 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: bdd06ff6d9eb41ce8dbeaa6633ede5c5     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  21:23:35 up  9:56,  4 users,  load average: 0.50, 0.55, 0.81

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -184,7 +184,7 @@ int main()
                     }
                 }
                 game_progress();
-                if (fail) break;
+                //if (fail) break;
                 screen_update();
                 next_frame += 1000 / FPS;
         }
