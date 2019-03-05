de691b09ebda09bfb71494345632419e99c1c5ea
tracer-nju <tracer@nju.edu.cn>
2019-03-05 11:53:48 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: bdd06ff6d9eb41ce8dbeaa6633ede5c5     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  11:53:48 up  1:58,  4 users,  load average: 0.71, 0.67, 0.61

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -12,53 +12,66 @@ const int STEP = 5;
 static int width, height, next_frame, key;
 
 struct Player {
-    int dx, dy;
-    int x,y;
+        int dx, dy;
+        int x, y;
 } player;
 
 uint32_t black[1000];
-void clear_rect(int x, int y, int w, int h){
-    draw_rect(black, x, y, w, h);
+void clear_rect(int x, int y, int w, int h)
+{
+        draw_rect(black, x, y, w, h);
 }
 
-void game_progress(){
+void game_progress()
+{
 }
 
-int x=0, y=0;
-void screen_update(){
-  uint32_t pixels[100];
-  for (int i=0;i<100;++i) pixels[i] = 0xff00ff;
-  clear_rect(player.x, player.y, 10, 10);
-  UPDATE(player);
-  draw_rect(pixels, player.x, player.y, 10, 10);
+int x = 0, y = 0;
+void screen_update()
+{
+        uint32_t pixels[100];
+        for (int i = 0; i < 100; ++i)
+                pixels[i] = 0xff00ff;
+        clear_rect(player.x, player.y, 10, 10);
+        UPDATE(player);
+        draw_rect(pixels, player.x, player.y, 10, 10);
 }
 
-int main() {
-  // Operating system is a C program
-  _ioe_init();
-  width = screen_width();
-  height = screen_height();
+int main()
+{
+        // Operating system is a C program
+        _ioe_init();
+        width = screen_width();
+        height = screen_height();
 
-  player.x = width/2;
-  player.y = height/2;
+        player.x = width / 2;
+        player.y = height / 2;
 
-  while (1) {
-    while (uptime() < next_frame);
-    while ((key = read_key())!=_KEY_NONE && ISKEYDOWN(key)){
-    printf("%d\n", key);
-        switch KEYCODE(key){
-          case _KEY_W: player.dy=-5; break;
-          case _KEY_S: player.dy=5; break;
-          case _KEY_A: player.dx=-5; break;
-          case _KEY_D: player.dx=5; break;
-          default: break;
+        while (1) {
+                while (uptime() < next_frame) ;
+                while ((key = read_key()) != _KEY_NONE && ISKEYDOWN(key)) {
+                        switch KEYCODE
+                                (key) {
+                        case _KEY_W:
+                                player.dy = -5;
+                                break;
+                        case _KEY_S:
+                                player.dy = 5;
+                                break;
+                        case _KEY_A:
+                                player.dx = -5;
+                                break;
+                        case _KEY_D:
+                                player.dx = 5;
+                                break;
+                        default:
+                                break;
+                                }
+                }
+                game_progress();
+                screen_update();
+                next_frame += 1000 / FPS;
         }
-    }
-    game_progress();
-    screen_update();
-    next_frame += 1000/FPS;
-  }
 
-  return 0;
+        return 0;
 }
-
