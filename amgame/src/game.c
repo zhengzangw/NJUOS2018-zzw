d457969cf261950b163296e87e6c16d3d1704b73
tracer-nju <tracer@nju.edu.cn>
2019-03-05 13:57:38 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: bdd06ff6d9eb41ce8dbeaa6633ede5c5     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  13:57:38 up  3:24,  4 users,  load average: 0.75, 0.45, 0.33

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -77,7 +77,7 @@ void clear_rect(int x, int y, int w, int h)
 
 void screen_update_player(){
     int sw = 0;
-    if (player.dy==-VECT) sw=1;
+    if (player.dy<0) sw=1;
     clear_rect(player.x, player.y, 20, 20);
     UPDATE(player);
     draw_rect(player_pixels[sw], player.x, player.y, 20, 20);
