539eaadade6cb787d7db0b89bd2a20dbae557491
tracer-nju <tracer@nju.edu.cn>
2019-03-09 20:35:11 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  20:35:11 up 14:45,  4 users,  load average: 0.28, 0.14, 0.10

--- libco/co.c
+++ libco/co.c
@@ -21,55 +21,52 @@ struct co {
   jmp_buf env;
   char stack[32 KB];
   char done;
-  void *stack_back;
+  void *stackptr;
 };
-struct co coroutines[MAX_CO];
-int co_num, cur_co;
+struct co crs[MAX_CO];
+int co_num, cur;
 
-#define mainco coroutines[0]
-#define changeframe(num)\
+#define changeframe(old, new)\
   asm volatile("mov " SP ", %0; mov %1, " SP :\
-               "=g"(coroutines[num].stack_back):\
-               "g"(START_OF_STACK(coroutines[num].stack)))
+               "=g"(crs[old].stackptr):\
+               "g"(crs[new].stackptr))
 #define restoreframe(num)\
-  asm volatile("mov %0," SP : : "g"(coroutines[num].stack_back))
+  asm volatile("mov %0," SP : : "g"(crs[num].stackptr))
 
 
 void co_init() {
-  strcpy(coroutines[0].name, "main");
-  co_num = cur_co = 0;
+  strcpy(crs[0].name, "main");
+  co_num = cur = 0;
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  coroutines[++co_num].done = 0;
-  strcpy(coroutines[co_num].name, name);
+  crs[++co_num].done = 0;
+  strcpy(crs[co_num].name, name);
 
-  int ind = setjmp(mainco.env);
+  int ind = setjmp(crs[cur].env);
   if (!ind){
-    cur_co = co_num;
-    changeframe(co_num);
+    changeframe(cur,co_num);
+    cur = co_num;
     func(arg); // Test #2 hangs
-    coroutines[co_num].done = 1;
+    crs[co_num].done = 1;
   }
-  printf("%p %p\n", &ind, coroutines[co_num].stack_back);
-  //restoreframe(co_num);
+  restoreframe(cur);
 
-  return &(coroutines[co_num]);
+  return &(crs[co_num]);
 }
 
 void co_yield() {
   int id = rand()%(co_num+1);
 
-  int ind = setjmp(coroutines[cur_co].env);
-  printf("ind = %d\n", ind);
+  int ind = setjmp(crs[cur].env);
+  //printf("ind = %d\n", ind);
   if (!ind){
-        printf("bef jmp, cur_co=%d, id=%d\n", cur_co, id);
-        cur_co = id;
-        changeframe(id);
-        longjmp(coroutines[id].env, 1);
+        changeframe(cur, id);
+        cur = id;
+        longjmp(crs[id].env, 1);
   }
-  restoreframe(id);
-  printf("End of yield\n");
+  restoreframe(cur);
+  //printf("End of yield\n");
 }
 
 void co_wait(struct co *thd) {
