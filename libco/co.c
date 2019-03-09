425bd60acf8813869f10350109965c3d705d88ab
tracer-nju <tracer@nju.edu.cn>
2019-03-09 20:53:29 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  20:53:29 up 15:04,  4 users,  load average: 0.31, 0.28, 0.25

--- libco/co.c
+++ libco/co.c
@@ -43,11 +43,12 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   crs[++co_num].done = 0;
   crs[co_num].stackptr = crs[co_num].stack;
   strcpy(crs[co_num].name, name);
+  void *arg_ = arg;
+  func_t func_ = func;
 
   printf("cur = %d\n", cur);
   int ind = setjmp(crs[cur].env);
   if (!ind){
-    printf("bef: %s, %p\n", (char *)arg, func);
     changeframe(cur,co_num);
     cur = co_num;
     printf("bef: %s, %p\n", (char *)arg, func);
