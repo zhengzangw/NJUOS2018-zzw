f0fc666af42c582b78d24c3665edbc7e249de26c
tracer-nju <tracer@nju.edu.cn>
2019-03-09 17:10:06 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  17:10:07 up 11:45,  4 users,  load average: 0.23, 0.30, 0.14

--- libco/co.c
+++ libco/co.c
@@ -1,20 +1,76 @@
 #include <stdio.h>
+#include <setjmp.h>
+#include <string.h>
+#include <stdlib.h>
 #include "co.h"
 
+#define KB * 1024LL
+#define MB KB * 1024LL
+#define GB MB * 1024LL
+#define MAX_CO 10
+#define START_OF_STACK(stack) ((stack)+sizeof(stack))
+
+#if defined(__i386__)
+  #define SP "%%esp"
+#elif defined(__x86_64__)
+  #define SP "%%rsp"
+#endif
+
+void *main_stack;
+jmp_buf main_env;
+
 struct co {
+  char name[64];
+  jmp_buf env;
+  char stack[32 KB];
+  char done;
 };
+static struct co coroutines[MAX_CO];
+static int co_num;
+
+static inline void changeframe(void * stack){
+  asm volatile("mov %1, " SP :
+               :
+               "g"(stack));
+}
 
 void co_init() {
+  asm volatile("mov " SP ", %0" :
+               "=g"(main_stack));
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  func(arg); // Test #2 hangs
-  return NULL;
+  coroutines[co_num].done = 0;
+  strcpy(coroutines[co_num].name, name);
+
+  int ind = setjmp(main_env);
+  if (ind){
+    changeframe(START_OF_STACK(coroutines[co_num].stack));
+    func(arg); // Test #2 hangs
+    coroutines[co_num].done = 1;
+  }
+  co_num++;
+  return &(coroutines[co_num-1]);
 }
 
 void co_yield() {
+  int id = rand()%(co_num+1);
+
+  int ind = setjmp(coroutines[co_num-1].env);
+  if (!ind){
+      if (!id){
+        changeframe(main_stack);
+        longjmp(main_env, 1);
+      } else {
+        changeframe(START_OF_STACK(coroutines[id-1].stack));
+        longjmp(coroutines[id-1].env, 1);
+      }
+  }
 }
 
 void co_wait(struct co *thd) {
+    if (!thd->done) {
+      co_yield();
+    }
 }
 
