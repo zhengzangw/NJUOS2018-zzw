f66cf4411f4e18cdcf510bdf89ec5bf001d72b26
tracer-nju <tracer@nju.edu.cn>
2019-03-09 19:55:37 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  19:55:37 up 14:06,  4 users,  load average: 0.02, 0.12, 0.36

--- libco/co.c
+++ libco/co.c
@@ -51,7 +51,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
     func(arg); // Test #2 hangs
     coroutines[co_num].done = 1;
   }
-  restoreframe(0);
+  restoreframe(co_num);
 
   return &(coroutines[co_num]);
 }
