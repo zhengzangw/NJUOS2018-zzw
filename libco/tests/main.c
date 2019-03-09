1978e8ead0503a142e22a2cfea9b7babdaed01a7
tracer-nju <tracer@nju.edu.cn>
2019-03-10 00:20:13 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  00:20:13 up 18:30,  4 users,  load average: 0.51, 0.43, 0.56

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -103,7 +103,7 @@ static void test_2() {
     Queue *queue = q_new();
 
     struct co *thd1 = co_start("producer-1", producer, queue);
-    struct co *thd2 = co_start("producer-2", producer, queue);
+    //struct co *thd2 = co_start("producer-2", producer, queue);
     struct co *thd3 = co_start("consumer-1", consumer, queue);
     struct co *thd4 = co_start("consumer-2", consumer, queue);
 
