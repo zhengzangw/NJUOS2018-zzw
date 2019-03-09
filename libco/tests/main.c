fe564fa23baf428814ce381042e1456292691c69
tracer-nju <tracer@nju.edu.cn>
2019-03-10 00:59:14 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  00:59:14 up 19:09,  4 users,  load average: 0.18, 0.38, 0.46

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -17,8 +17,7 @@ static int get_count() {
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     char tmp[20];
-    //sprintf(tmp, "%s", s);
-    strcpy(tmp, s);
+    sprintf(tmp, "%s", s);
     for (int i = 0; i < 100; ++i) {
         printf("%s%d ", tmp, get_count());
         add_count();
