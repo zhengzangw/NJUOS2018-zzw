bb9b2f2f104996345c041ab880161aef2bc41c2d
tracer-nju <tracer@nju.edu.cn>
2019-03-09 20:48:27 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  20:48:27 up 14:59,  4 users,  load average: 0.77, 0.44, 0.27

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -19,6 +19,7 @@ static void work_loop(void *arg) {
     for (int i = 0; i < 100; ++i) {
         printf("%s%d  \n", s, get_count());
         add_count();
+        printf("before yield\n");
         co_yield();
         printf("End of co_yield\n");
     }
