5bdff7580f29d020276a68d9a32542cf1dc33563
tracer-nju <tracer@nju.edu.cn>
2019-03-10 00:24:58 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  00:24:58 up 18:35,  4 users,  load average: 0.25, 0.41, 0.51

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -31,7 +31,7 @@ static void test_1() {
 
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
-    struct co *thd3 = co_start("thread-3", work, "Y");
+    struct co *thd3 = co_start("thread-3", work, "Z");
 
     co_wait(thd1);
     co_wait(thd2);
