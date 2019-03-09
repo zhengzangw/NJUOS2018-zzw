8aec158153b96f0235c6e4d0ac50a9e2045e3c80
tracer-nju <tracer@nju.edu.cn>
2019-03-09 18:51:51 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  18:51:51 up 13:02,  4 users,  load average: 0.02, 0.05, 0.08

--- libco/co.c
+++ libco/co.c
@@ -54,7 +54,6 @@ struct co* co_start(const char *name, func_t func, void *arg) {
    // changeframe(START_OF_STACK(coroutines[co_num].stack));
     //printf("%p %p %p\n",coroutines, coroutines[co_num].stack, START_OF_STACK(coroutines[co_num].stack));
     //printf("*******************\n");
-    char temp[10] = "x";
     func(arg); // Test #2 hangs
     coroutines[co_num].done = 1;
   }
