1e554f4ee3cf23fb02687cb6bfd9eed49e7641d7
tracer-nju <tracer@nju.edu.cn>
2019-03-10 00:20:46 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 1643bbc303014ba1ae57c3a6dc1bc3c9            Boot ID: ce37cc3f133246dab5f8d42d2bff534f     Virtualization: oracle   Operating System: Ubuntu 18.10             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  00:20:46 up 18:31,  4 users,  load average: 0.45, 0.42, 0.55

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -57,10 +57,6 @@ static void do_produce(Queue *queue) {
     }
     memset(item->data, 0, 10);
     sprintf(item->data, "libco\n");
-    printf("%s\n", (char *)item->data);
-    printf("%p\n", &item);
-
-    sprintf(item->data, "libco-%d", g_count++);
     q_push(queue, item);
 }
 
