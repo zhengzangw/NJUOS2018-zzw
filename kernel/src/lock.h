7ffcdd383df44dadd72013e6255ca6e27c136272
tracer-nju <tracer@nju.edu.cn>
2019-03-24 18:18:53 +0800

    Static hostname: zhengzangw-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: d0571d243ced4e41abfd8a4662b38c9f            Boot ID: afae1ebd085e452e8e414cecd04ee942   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  18:18:53 up  4:58,  1 user,  load average: 0.66, 0.44, 0.28

--- kernel/src/lock.h
+++ kernel/src/lock.h
@@ -2,29 +2,16 @@ typedef struct __lock_t {int flag;} lock_t;
 void cli() { __asm__ __volatile__ ("cli");}
 void sti() { __asm__ __volatile__ ("sti");}
 
-char CompareAndSwap(int *ptr, int old, int nov){
-    unsigned char ret;
-    __asm__ __volatile__ (
-        " lock\n"
-        " cmpxchgl %2, %1\n"
-        " sete %0\n"
-        : "=q" (ret), "=m" (*ptr)
-        : "r" (nov), "m" (*ptr), "a"(old)
-        : "memory"
-    );
-    return ret;
-}
-
 void init(lock_t *mutex) {
     mutex->flag = 0;
 }
 
 void lock(lock_t *mutex) {
     cli();
-    while (CompareAndSwap(&mutex->flag, 0, 1)==1);
+    while (_atomic_xchg(&mutex->flag, 1));
 }
 
 void unlock(lock_t *mutex){
-    mutex->flag = 0;
+    _atomic_xchg(&mutex->flag, 0);    
     sti();
 }
