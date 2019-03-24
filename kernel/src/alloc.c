ac327b111e58f27da59c042bb5c9848d2a68f7da
tracer-nju <tracer@nju.edu.cn>
2019-03-24 20:10:50 +0800

    Static hostname: zhengzangw-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: d0571d243ced4e41abfd8a4662b38c9f            Boot ID: afae1ebd085e452e8e414cecd04ee942   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  20:10:50 up  6:50,  1 user,  load average: 0.77, 0.90, 0.89

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -3,11 +3,17 @@
 #include <lock.h>
 
 static uintptr_t pm_start, pm_end;
+static lock_t debug_lock;
 
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
 
+  init(&debug_lock);
+
+  lock(&debug_lock);
+  printf("pm_start = %p\npm_end = %p\nsize of heap=%p\n", pm_start, pm_end, pm_end-pm_start);
+  unlock(&debug_lock);
 }
 
 static void *kalloc(size_t size) {
