32c5ddbc9f1c0a0d00a371283c7aabbfcdb67e5a
tracer-nju <tracer@nju.edu.cn>
2019-03-25 00:00:37 +0800

    Static hostname: zhengzangw-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: d0571d243ced4e41abfd8a4662b38c9f            Boot ID: afae1ebd085e452e8e414cecd04ee942   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  00:00:38 up 10:40,  1 user,  load average: 1.08, 0.58, 0.49

--- kernel/src/list.c
+++ kernel/src/list.c
@@ -1,8 +1,8 @@
 #include <list.h>
 
 void list_init(uintptr_t pm_start, uintptr_t pm_end){
-    head = (char *)pm_start;
-    tail = (char *)(pm_end-BIAS);
+    head = (void *)pm_start;
+    tail = (void *)(pm_end-BIAS);
     head->next = tail; head->pre = NULL;
     tail->next = NULL; tail->pre = head;
     head->start = pm_start; head->end = head->start+BIAS;
