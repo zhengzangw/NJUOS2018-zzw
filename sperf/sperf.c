1935f3d81264a6d56d8d7c3d92e8da7c9af1360f
tracer-nju <tracer@nju.edu.cn>
2019-03-27 22:38:01 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  22:38:01 up  2:56,  4 users,  load average: 0.20, 1.09, 1.11

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *nargv[256];
 int main(int argc, char *argv[]) {
-  char *nargv[256];
   nargv[0] = "/usr/bin/strace";
   for (int i=1;i<argc;++i) nargv[i] = argv[i];
   if (execve("/usr/bin/strace", nargv, NULL)==-1){
