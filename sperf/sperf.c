cfbc0ff4ea46f3bfadd4faf85791a2bbba646f99
tracer-nju <tracer@nju.edu.cn>
2019-03-27 22:24:24 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  22:24:24 up  2:43,  4 users,  load average: 0.37, 0.59, 0.42

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,11 +5,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   char nargv[256][256];
-  nargv[0] = "strace";
-  for (int i=1;i<argc;++i) nargv[i] = argv[i];
+  strcpy(nargv[0],"strace");
+  for (int i=1;i<argc;++i) strcpy(nargv[i],argv[i]);
   nargv[argc] = NULL;
   for (int i=0;nargv[i]!=NULL;++i){
       printf("%s\n", nargv[i]);
