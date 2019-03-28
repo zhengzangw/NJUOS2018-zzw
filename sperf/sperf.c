008014c7225305777393b4c9db3c96c8426c0424
tracer-nju <tracer@nju.edu.cn>
2019-03-28 21:07:11 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  21:07:11 up 1 day, 32 min,  4 users,  load average: 0.61, 0.21, 0.07

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,109 +8,115 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Syscall{
-  char name[64];
-  double time;
+struct Syscall {
+        char name[64];
+        double time;
 } info[1024];
 int h_info;
 char tmp[1024], ttmp[1024], name[1024];
 
-int loc(char *name){
-    int i;
-    for (i=0;i<h_info;++i){
-        if (strcmp(name, info[i].name)==0){
-            break;
+int loc(char *name)
+{
+        int i;
+        for (i = 0; i < h_info; ++i) {
+                if (strcmp(name, info[i].name) == 0) {
+                        break;
+                }
         }
-    }
-    if (i==h_info) {
-        strcpy(info[i].name, name);
-        h_info ++;
-    }
-    return i;
+        if (i == h_info) {
+                strcpy(info[i].name, name);
+                h_info++;
+        }
+        return i;
 }
 
 #define clear() printf("\e[2J\e[H\e[?25l")
 #define show() printf("\e[?25h")
 
-void draw_table(){
-  clear();
-  double sum = 0;
-  for (int i=0;i<h_info;++i){
-      fprintf(stdout, "%s: %10lf\n", info[i].name, info[i].time);
-      sum += info[i].time;
-  }
-  fprintf(stdout, "SUM: %10lf\n", sum);
+void draw_table()
+{
+        clear();
+        double sum = 0;
+        for (int i = 0; i < h_info; ++i) {
+                fprintf(stdout, "%s: %10lf\n", info[i].name, info[i].time);
+                sum += info[i].time;
+        }
+        fprintf(stdout, "SUM: %10lf\n", sum);
 }
 
+int main(int argc, char *argv[], char *env[])
+{
+        //new argv
+        char *argv_new[argc + 2];
+        argv_new[0] = "/usr/bin/strace";
+        argv_new[1] = "-Txx";
+        for (int i = 1; i < argc; ++i)
+                argv_new[i + 1] = argv[i];
+        argv_new[argc + 1] = NULL;
+        clock_t begin = clock();
 
-int main(int argc, char *argv[], char *env[]) {
-  //new argv
-  char *argv_new[argc+2];
-  argv_new[0] = "/usr/bin/strace";
-  argv_new[1] = "-Txx";
-  for (int i=1;i<argc;++i) argv_new[i+1] = argv[i];
-  argv_new[argc+1] = NULL;
-  clock_t begin = clock();
-
-  int flides[2];
-  if (pipe(flides)!=0){
-      perror("pipe failed");
-      exit(EXIT_FAILURE);
-  }
-  int pid = fork();
-  if (pid == 0){
-     dup2(flides[1], STDERR_FILENO);
-     close(STDOUT_FILENO);
-     execve("/usr/bin/strace", argv_new, env);
-  } else {
-      FILE* input = fdopen(flides[0], "r");
-
-      clear();
-      while (true){
-
-        memset(tmp, 0, sizeof(tmp));
-        do {
-        fgets(ttmp, 1024, input);
-        strcat(tmp, ttmp);
-
-        if (strncmp(ttmp, "/usr/bin/strace", 15)==0){
-            printf("%s+++  Fail to run sperf +++\n", tmp);
-            show();
-            exit(1);
+        int flides[2];
+        if (pipe(flides) != 0) {
+                perror("pipe failed");
+                exit(EXIT_FAILURE);
         }
-        if (strncmp(ttmp, "+++", 3)==0){
-            exit(1);
-        }
-        } while (tmp[strlen(tmp)-2]!='>');
+        int pid = fork();
+        if (pid == 0) {
+                dup2(flides[1], STDERR_FILENO);
+                close(STDOUT_FILENO);
+                execve("/usr/bin/strace", argv_new, env);
+        } else {
+                FILE *input = fdopen(flides[0], "r");
 
-        int t;
-        for (t=0;t<strlen(tmp);++t){
-            if (tmp[t]=='(') break;
-        }
-        if (t==strlen(tmp)) {
-            printf("%s", tmp);
-assert(0);
-        }
-        strncpy(name, tmp, t);
-        name[t] = '\0';
+                clear();
+                while (true) {
 
-        double dur;
-        for (t=strlen(tmp)-1;t>=0;--t){
-            if (tmp[t]=='<') break;
-        }
-        if (t<0) continue;
-        sscanf(tmp+t+1,"%lf", &dur);
-        info[loc(name)].time += dur;
+                        memset(tmp, 0, sizeof(tmp));
+                        do {
+                                fgets(ttmp, 1024, input);
+                                assert(ttmp !=NULL);
+                                strcat(tmp, ttmp);
 
-        clock_t now = clock();
-        if ((double)(now - begin)/CLOCKS_PER_SEC>=0.25){
-            draw_table();
-            begin = now;
-        }
-      }
-     draw_table();
-     show();
-  }
+                                if (strncmp(ttmp, "/usr/bin/strace", 15) == 0) {
+                                        printf("%s+++  Fail to run sperf +++\n",
+                                               tmp);
+                                        show();
+                                        exit(1);
+                                }
+                                if (strncmp(ttmp, "+++", 3) == 0) {
+                                        exit(1);
+                                }
+                        } while (tmp[strlen(tmp) - 2] != '>');
 
-  return 0;
+                        int t;
+                        for (t = 0; t < strlen(tmp); ++t) {
+                                if (tmp[t] == '(')
+                                        break;
+                        }
+                        if (t == strlen(tmp))
+                                continue;
+                        strncpy(name, tmp, t);
+                        name[t] = '\0';
+
+                        double dur;
+                        for (t = strlen(tmp) - 1; t >= 0; --t) {
+                                if (tmp[t] == '<')
+                                        break;
+                        }
+                        if (t < 0)
+                                continue;
+                        sscanf(tmp + t + 1, "%lf", &dur);
+                        info[loc(name)].time += dur;
+
+                        clock_t now = clock();
+                        if ((double)(now - begin) / CLOCKS_PER_SEC >= 0.25) {
+                                draw_table();
+                                begin = now;
+                        }
+                }
+                draw_table();
+                show();
+        }
+
+        return 0;
 }
