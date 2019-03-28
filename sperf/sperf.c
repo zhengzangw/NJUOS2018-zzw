8a643a00a630b53d252e069b012911399c17861c
tracer-nju <tracer@nju.edu.cn>
2019-03-28 14:21:25 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  14:21:25 up 18:20,  4 users,  load average: 0.39, 0.14, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -7,15 +7,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *nargv[256];
 int main(int argc, char *argv[], char *env[]) {
-  printf("%d\n", argc);
-  nargv[0] = "/usr/bin/strace";
-  for (int i=1;i<argc;++i) nargv[i] = argv[i];
-  nargv[argc] = NULL;
-  if (execve("/usr/bin/strace", nargv, env)==-1){
-     perror("Execve Failed!");
-     exit(1);
+  //new argv
+  char *argv_new[argc+2];
+  argv_new[0] = "/usr/bin/strace";
+  argv_new[1] = "-Txx";
+  for (int i=1;i<argc;++i) argv_new[i+1] = argv[i];
+  argv_new[argc+1] = NULL;
+
+  int flides[2];
+  if (pipe(flides)!=0){
+      perror("pipe failed");
+      exit(EXIT_FAILURE);
   }
+  int pid = fork();
+  if (pid == 0){
+     close(1);
+     dup2(flides[1], 1);
+     execve("/usr/bin/strace", argv_new, env);
+  } else {
+     close(0);
+     dup2(flides[0], 0);
+     execlp("wc", "wc", "-l", NULL);
+  }
+
   return 0;
 }
