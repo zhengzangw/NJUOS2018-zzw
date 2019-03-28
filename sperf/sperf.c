0c1161684f60324e30b3cd1418bb4cad8fa8ab3a
tracer-nju <tracer@nju.edu.cn>
2019-03-28 21:41:30 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  21:41:30 up 1 day,  1:07,  4 users,  load average: 0.59, 0.59, 0.48

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -33,7 +33,7 @@ int loc(char *name)
         return i;
 }
 
-#define clear() printf("\e[c\e[H\e[?25l")
+#define clear() printf("\e[2J\e[H\e[?25l")
 #define show() printf("\e[?25h")
 
 void sort(){
@@ -48,7 +48,7 @@ void sort(){
 
 void draw_table()
 {
-        clear();
+        printf("\e[c");
         double sum = 0;
         for (int i = 0; i < h_info; ++i) {
                 fprintf(stdout, "%s: %10lf\n", info[i].name, info[i].time);
