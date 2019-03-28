cd5b081af13085d6204b6655b56ea1e6d5777d46
tracer-nju <tracer@nju.edu.cn>
2019-03-28 14:44:13 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  14:44:13 up 18:43,  4 users,  load average: 0.08, 0.14, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -25,11 +25,11 @@ int main(int argc, char *argv[], char *env[]) {
   int pid = fork();
   if (pid == 0){
      dup2(flides[1], STDOUT_FILENO);
-     close(flides[0]);
      //execve("/usr/bin/strace", argv_new, env);
      execlp("ls", "ls", "-1", NULL);
   } else {
      dup2(flides[0], STDIN_FILENO);
+     close(flides[1]);
      execlp("wc", "wc", "-l", NULL);
   }
 
