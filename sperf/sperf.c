cae312c65e7acd51bc223f1c93d198dda5bc5c89
tracer-nju <tracer@nju.edu.cn>
2019-03-28 14:22:28 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  14:22:28 up 18:21,  4 users,  load average: 0.13, 0.11, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -9,11 +9,13 @@
 
 int main(int argc, char *argv[], char *env[]) {
   //new argv
+  /*
   char *argv_new[argc+2];
   argv_new[0] = "/usr/bin/strace";
   argv_new[1] = "-Txx";
   for (int i=1;i<argc;++i) argv_new[i+1] = argv[i];
   argv_new[argc+1] = NULL;
+  */
 
   int flides[2];
   if (pipe(flides)!=0){
@@ -25,7 +27,7 @@ int main(int argc, char *argv[], char *env[]) {
      close(1);
      dup2(flides[1], 1);
      //execve("/usr/bin/strace", argv_new, env);
-     exclp("ls", "ls", -1, NULL);
+     execlp("ls", "ls", -1, NULL);
   } else {
      close(0);
      dup2(flides[0], 0);
