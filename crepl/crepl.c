4df8e5e384e7ab70bec3fb99599036aad4d72443
tracer-nju <tracer@nju.edu.cn>
2019-03-31 20:34:15 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  20:34:15 up 3 days, 15:11,  7 users,  load average: 0.11, 0.17, 0.17

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -13,10 +13,8 @@ int main(int argc, char *argv[], char *env[]) {
       printf(">> ");
       scanf("%s", buf);
 
-      char name[]="tmpfileXXXXXX";
-      int fd=mkstemp(name);
-      printf("%d %s\n",fd, name);
-      assert(0);
+      char tmpname[]="tmpfileXXXXXX";
+      int fd=mkstemp(tmpname);
 
       int pid = fork();
       int status;
@@ -24,7 +22,7 @@ int main(int argc, char *argv[], char *env[]) {
         printf("Child:\n");
         char *argv_new[3];
         argv_new[0] = "/bin/cp";
-        argv_new[1] = name;
+        argv_new[1] = tmpname;
         argv_new[2] = "my.txt";
         execve("/bin/cp", argv_new, env);
       } else {
@@ -33,6 +31,7 @@ int main(int argc, char *argv[], char *env[]) {
         printf("child's pid=%d, exit = %d", pid_ch, ret);
       }
 
+      unlink(tmpname);
 
       printf("  %s\n", buf);
     }
