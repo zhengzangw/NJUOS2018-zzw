f9c9286f66c2b76a9ac356b8846d69eb4da5ae6d
tracer-nju <tracer@nju.edu.cn>
2019-04-01 16:37:07 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  16:37:07 up 3 days, 22:48,  7 users,  load average: 0.29, 0.28, 0.22

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[], char *env[])
                 bzero(buf, sizeof(buf));
                 fgets(buf, 10000, stdin);
                 if (feof(stdin)) {
-                    printf("Ended...");
+                    printf("\033[31mEnded...\033[0m\n");
                     fflush(stdout);
                     break;
                 }
