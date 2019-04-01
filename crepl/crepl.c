c526235645c37200f1ebcb3861b47d48559e6cf9
tracer-nju <tracer@nju.edu.cn>
2019-04-01 15:27:09 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 33ae6ddad03c4c33a613dc2950aca136     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  15:27:09 up 3 days, 21:38,  7 users,  load average: 0.23, 0.22, 0.24

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -11,7 +11,6 @@
 #include <time.h>
 
 char buf[20000], buf2[20000], funcname[100];
-char tmpfuncname[200];
 char *argv_new[20];
 bool isfunc;
 char wrapper[] = "__expr_wrap_";
@@ -19,8 +18,17 @@ int numfunc;
 
 int main(int argc, char *argv[], char *env[])
 {
-
         srand(time(NULL));
+        argv_new[0] = "/usr/bin/gcc";
+        argv_new[1] = sizeof(void *) == 4 ? "-m32" : "-m64";
+        argv_new[2] = "-x";
+        argv_new[3] = "c";
+        argv_new[4] = "-fPIC";
+        argv_new[5] = "-shared";
+        argv_new[7] = "-o";
+        argv_new[9] = "-w";
+        argv_new[10] = NULL;
+
         while (true) {
                 printf(">> ");
                 isfunc = 0;
@@ -28,26 +36,21 @@ int main(int argc, char *argv[], char *env[])
                 //Get input
                 bzero(buf, sizeof(buf));
                 fgets(buf, 10000, stdin);
-                if (feof(stdin))
-                        break;
-                int b;
+                if (feof(stdin)) break;
+                int b, s, t;
                 for (b = 0; buf[b] == ' '; ++b) ;
-                if (strcmp(buf + b, "exit\n") == 0)
-                        return 0;
+                if (strcmp(buf + b, "exit\n") == 0) return 0;
                 if (strncmp(buf + b, "int", 3) == 0) {
-                        puts("isfunc");
                         bzero(funcname, sizeof(funcname));
                         isfunc = 1;
-                        int s, t;
                         for (s = b + 3; buf[s] == ' '; ++s) ;
                         for (t = s; buf[t] != '('; ++t) ;
                         strncpy(funcname, buf + s, t - s);
                 } else {
                         buf[strlen(buf) - 1] = '\0';
                         bzero(buf2, sizeof(buf2));
-                        sprintf(tmpfuncname, "%s%d", wrapper, numfunc++);
-                        sprintf(buf2, "int %s(){return (%s);}", tmpfuncname,
-                                buf);
+                        sprintf(funcname, "%s%d", wrapper, numfunc++);
+                        sprintf(buf2, "int %s(){return (%s);}", funcname, buf);
                         printf("%s\n", buf2);
                 }
 
@@ -59,22 +62,13 @@ int main(int argc, char *argv[], char *env[])
                 //Prepare Varible
                 char tmpo[30];
                 sprintf(tmpo, "./%s.so", tmpname);
-                argv_new[0] = "/usr/bin/gcc";
-                argv_new[1] = sizeof(void *) == 4 ? "-m32" : "-m64";
-                argv_new[2] = "-x";
-                argv_new[3] = "c";
-                argv_new[4] = "-fPIC";
-                argv_new[5] = "-shared";
                 argv_new[6] = tmpname;
-                argv_new[7] = "-o";
                 argv_new[8] = tmpo;
-                argv_new[9] = "-w";
-                argv_new[10] = NULL;
 
                 int pid = fork();
                 int status;
                 if (pid == 0) {
-                        //close(STDERR_FILENO);
+                        close(STDERR_FILENO);
                         execve(argv_new[0], argv_new, env);
                 } else {
                         //wait
@@ -82,26 +76,20 @@ int main(int argc, char *argv[], char *env[])
                         int ret = WEXITSTATUS(status);
 
                         // Deal with result
-                        if (ret != 0)
-                                printf("  \033[31mCompile Error!\033[0m\n");
+                        if (ret != 0) printf("  \033[31mCompile Error!\033[0m\n");
                         else {
                                 int (*dfunc) (void);
                                 void *dhandle =
                                     dlopen(tmpo, RTLD_NOW | RTLD_GLOBAL);
                                 if (dhandle == NULL) {
-                                        printf("\033[31mCompile Error:\033[0m%s\n", dlerror());
+                                        printf("  \033[31mCompile Error:\033[0m%s\n", dlerror());
                                 } else {
-
                                         if (isfunc) {
-                                                printf
-                                                    ("  \033[32mAdded:\033[0m %s",
-                                                     buf);
+                                                printf("  \033[32mAdded:\033[0m %s", buf);
                                         } else {
-                                                dfunc =
-                                                    dlsym(dhandle, tmpfuncname);
+                                                dfunc = dlsym(dhandle, funcname);
                                                 assert(dfunc != NULL);
-                                                printf("  (%s) = %d.\n", buf,
-                                                       dfunc());
+                                                printf("  (%s) = %d.\n", buf, dfunc());
                                         }
                                         dlclose(dhandle);
                                 }
