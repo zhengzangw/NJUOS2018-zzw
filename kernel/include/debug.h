a00c8651a845cfc76f21f102f333a14c8fc3bfcd
tracer-nju <tracer@nju.edu.cn>
2019-05-05 14:29:36 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 57a5c13473024893a8511d6bcd55fb8e     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  14:29:36 up  4:01,  5 users,  load average: 0.54, 0.92, 0.70

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -5,6 +5,7 @@
 
 #define DEBUG
 #define DEBUG_LOCK
+#define DEBUG_PMM
 
 // ========== General ============
 
@@ -47,7 +48,7 @@ extern spinlock_t lock_debug;
 #define Logcpu() Log("cpu #%c:\n", "12345678"[_cpu()])
 
 // ========== LIST ===========
-#define Lognode(node) Log("node: start=%p, end=%p\n", node->start, node->end)
+#define Lognode(node, tag) Log(tag "node: start=%p, end=%p\n", node->start, node->end)
 // ========== SPINLOCK =======
 #define FL_IF 0x00000200
 #define assertIF1() Assert((readflags()&FL_IF)==0, "interruptible where IF should be 0")
