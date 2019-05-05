1fabacf8b86cf092b335505bca517c45dbc37cb5
tracer-nju <tracer@nju.edu.cn>
2019-05-06 00:23:18 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 57a5c13473024893a8511d6bcd55fb8e     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  00:23:18 up 12:42,  5 users,  load average: 0.42, 0.30, 0.40

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -12,9 +12,9 @@ extern spinlock_t lock_print;
     kmt->spin_lock(&lock_print);\
     printf(format, __VA_ARGS__); \
     kmt->spin_unlock(&lock_print)
-#define warning(format, ...) \
+#define warning(format) \
     kmt->spin_lock(&lock_print);\
-    printf("\033[33m" format "\033[0m", __VA_ARGS__);\
+    printf(format); \
     kmt->spin_unlock(&lock_print)
 
 #endif
