4b3d064c602224d02510ab7cd672149e1e92cb16
tracer-nju <tracer@nju.edu.cn>
2019-05-05 14:16:27 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 57a5c13473024893a8511d6bcd55fb8e     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  14:16:27 up  3:47,  5 users,  load average: 0.38, 0.28, 0.24

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -17,10 +17,10 @@
           __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 #else
 #define Log(format, ...) \
-    kmt->spin_lock(lock_debug); \
+    kmt->spin_lock(&lock_debug); \
     printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
           __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
-    kmt->spin_unlock(lock_debug)
+    kmt->spin_unlock(&lock_debug)
 #endif
 #endif
 
