b36a852170147742a05cff9fbb59b98fc772a92a
tracer-nju <tracer@nju.edu.cn>
2019-05-06 23:59:07 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f83055eb171b4f4aaa78af2b507ad179     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  23:59:07 up 10:16,  5 users,  load average: 0.38, 0.68, 0.55

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -130,7 +130,7 @@ void spin_init(spinlock_t *lk, const char *name){
 
 void spin_lock(spinlock_t *lk){
     pushcli();
-    //printf("L%c %s %d\n","12345678"[_cpu()], lk->name, readflags()&FL_IF);
+    printf("L%c %s %d\n","12345678"[_cpu()], lk->name, readflags()&FL_IF);
     Assert(!holding(lk), "locking a locked lock %s", lk->name);
 
     while (_atomic_xchg(&lk->locked, 1)!=0){};
@@ -146,7 +146,7 @@ void spin_unlock(spinlock_t *lk) {
     __sync_synchronize();
 
     asm volatile ("movl $0, %0" : "+m" (lk->locked) :);
-    //printf("U%c %s %d\n","12345678"[_cpu()], lk->name, readflags()&FL_IF);
+    printf("U%c %s %d\n","12345678"[_cpu()], lk->name, readflags()&FL_IF);
     popcli();
 }
 
