9b9067c2d980c8072b3edfb9ca489f2626ac65a0
tracer-nju <tracer@nju.edu.cn>
2019-05-07 02:03:33 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f83055eb171b4f4aaa78af2b507ad179     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  02:03:33 up 12:21,  5 users,  load average: 1.86, 1.51, 1.02

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -82,7 +82,7 @@ static void os_on_irq(int seq, int event, handler_t handler) {
 static _Context *os_trap(_Event ev, _Context *context) {
   Log("%d: %s", ev.event, ev.msg);
   assertIF0();
-  Assert(timelock[_cpu()]>=0, "timelock<0");
+  Assert(timelock[_cpu()]==0, "timelock<0");
   //Special Check
   switch (ev.event){
     case _EVENT_ERROR:
