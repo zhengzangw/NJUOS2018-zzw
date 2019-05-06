b2d50d35bf133953e6af8571d180d0fbb5743a70
tracer-nju <tracer@nju.edu.cn>
2019-05-06 11:26:03 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 57a5c13473024893a8511d6bcd55fb8e     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  11:26:03 up 23:44,  5 users,  load average: 0.17, 0.28, 0.36

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -131,7 +131,7 @@ void spin_init(spinlock_t *lk, const char *name){
 
 void spin_lock(spinlock_t *lk){
     pushcli();
-    printf("L %s", lk->name);
+    printf("L%c %s","012345678"[_cpu()], lk->name);
     Assert(!holding(lk), "locking a locked lock %s", lk->name);
 
     while (_atomic_xchg(&lk->locked, 1)!=0){
@@ -144,7 +144,7 @@ void spin_lock(spinlock_t *lk){
 }
 
 void spin_unlock(spinlock_t *lk) {
-    printf("U %s", lk->name);
+    printf("U%c %s","012345678"[_cpu()], lk->name);
     Assert(holding(lk), "release an unlocked lock %s", lk->name);
     lk->cpu = 0;
 
