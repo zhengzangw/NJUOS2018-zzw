999f240f74da9aad7bee69a04f2a6891a434386a
tracer-nju <tracer@nju.edu.cn>
2019-05-06 13:04:34 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f83055eb171b4f4aaa78af2b507ad179     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  13:04:34 up 34 min,  5 users,  load average: 0.54, 0.31, 0.32

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -28,6 +28,9 @@ _Context *kmt_context_switch(_Event ev, _Context * context){
       for (int i=0;i<MAXTASK;++i){
         int next = (cur+i+1)%MAXTASK;
         Log("Strange");
+        if (tasks[next]!=NULL||tasks[next]->id==next) {
+           Panic("id(%d)!=index(%d)", tasks[next]->id, next);
+        }
         Log("sStrange");
         if (!empty(tasks[next])&&(tasks[next]->run==0||(tasks[next]->id==cur&&cputask[_cpu()]!=NULL))){
             cputask[_cpu()] = tasks[next];
