38089031b76f80b0c7ee5d29a443309ff75ddf5c
tracer-nju <tracer@nju.edu.cn>
2019-05-07 01:59:59 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f83055eb171b4f4aaa78af2b507ad179     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  01:59:59 up 12:17,  5 users,  load average: 2.81, 1.39, 0.87

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -12,7 +12,6 @@ int timelock[MAXCPU]; //judge nested timelock
 void logging(void *arg) {
   while (1){
     lprintf("%s", (char *)arg);
-    _yield();
   }
 }
 
@@ -81,7 +80,7 @@ static void os_on_irq(int seq, int event, handler_t handler) {
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
-  Log("%d: %s", ev.event, ev.msg);
+  //Log("%d: %s", ev.event, ev.msg);
   assertIF0();
   Assert(timelock[_cpu()]>=0, "timelock<0");
   //Special Check
