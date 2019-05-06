8f6699459d3485ceace13119dd707c2f06c4908b
tracer-nju <tracer@nju.edu.cn>
2019-05-06 11:18:25 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 57a5c13473024893a8511d6bcd55fb8e     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  11:18:25 up 23:37,  5 users,  load average: 0.29, 0.40, 0.43

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -47,9 +47,9 @@ _Context *kmt_context_switch(_Event ev, _Context * context){
 
 void init(){
     kmt->spin_init(&lock_kmt, "kmt");
+    _halt(1);
     os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save);
     os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
-    _halt(1);
     for (int i=0;i<MAXTASK;++i){
         tasks[i] = NULL;
     }
