991c62156772b7bc6024a3fbfcdba5a6a40e5ec5
zhengzangwei <zhengzangw@163.com>
2019-05-06 14:03:02 +0800

    Fix ud2

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -45,7 +45,6 @@ static void os_init() {
 static void os_run() {
   _intr_write(1);
   while (1) {
-    assertIF1();
     _yield();
     Panic("SHOULD NOT REACH HERE");
   }
@@ -59,6 +58,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
     warning("%s\n", ev.msg);
     _halt(1);
   }
+  Log("%d: %s", ev.event, ev.msg);
   assertIF1();
   _Context *ret = NULL;
   kmt->spin_lock(&lock_os);
