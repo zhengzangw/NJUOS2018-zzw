3219ef058aa6dfb66ee3187b2c27784a860be894
tracer-nju <tracer@nju.edu.cn>
2019-05-07 20:37:52 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 820685f3912b4fe4b6087e5ddb5f94b1     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  20:37:52 up  5:22,  9 users,  load average: 0.08, 0.29, 0.64

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -205,10 +205,15 @@ void sem_wait(sem_t *sem){
         cputask[_cpu()]->sleep = 1;
         sem_list_add(sem, cputask[_cpu()]);
         assert(sem->pcb);
+        cputask[_cpu()]->run = 0;
+        assert(cpuncli[_cpu()]==0);
         kmt->spin_unlock(&sem->lock);
 
-        assert(cpuncli[_cpu()]==0);
         _yield();
+
+        kmt->spin_lock(&sem->lock);
+        cputask[_cpu()]->run = 1;
+        kmt->spin_unlock(&sem->lock);
     } else {
       kmt->spin_unlock(&sem->lock);
     }
