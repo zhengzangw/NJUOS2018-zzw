a045b67595729e1c30b1f5cb5e3f93be756bf06e
tracer-nju <tracer@nju.edu.cn>
2019-05-07 22:49:23 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 820685f3912b4fe4b6087e5ddb5f94b1     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  22:49:23 up  7:33,  9 users,  load average: 0.57, 0.46, 0.35

--- kernel/src/devices/tty/tty.c
+++ kernel/src/devices/tty/tty.c
@@ -271,9 +271,9 @@ void echo_task(void *name){
     device_t *tty = dev_lookup(name);
     while (1){
         char line[128], text[128];
-        sprintf(text, "(%s) $ ", name); tty_write(tty, 0, text, 128);
+        sprintf(text, "(%s) $ ", name); tty_write(tty, 0, text, strlen(name));
         int nread = tty->ops->read(tty, 0, line, sizeof(line));
         line[nread-1] = '\0';
-        sprintf(text, "Echo: %s.\n", line); tty_write(tty, 0, text, 128);
+        sprintf(text, "Echo: %s.\n", line); tty_write(tty, 0, text, strlen(text));
     }
 }
