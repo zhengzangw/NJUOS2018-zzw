0f0977709074dd7c89ff86dfca414409de7a6fcd
tracer-nju <tracer@nju.edu.cn>
2019-05-09 22:16:13 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  22:16:13 up 13:41,  4 users,  load average: 0.37, 0.31, 0.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <malloc.h>
+#include <stdbool.h>
 
 int size;
 void *mmap_open(char *name){
@@ -66,20 +67,31 @@ struct SFILE {
 }__attribute__((packed));
 typedef struct SFILE sfile_t;
 
+bool isword(char *ptr){
+    if (*ptr>='a'&&*ptr<='z') return true;
+    if (*ptr>='A'&&*ptr<='Z') return true;
+    if (*ptr>='0'&&*ptr<='9') return true;
+    return false;
+}
+
 #define BYTE(i) (*((uint8_t *) ptr + i))
 #define LOGBYTE(i) printf("%02x\n", BYTE(i));
 int main(int argc, char *argv[]) {
   char *img_ptr = mmap_open(argv[1]);
   dbr_t *dbr = malloc(sizeof(dbr_t));
+  char *end_ptr = img_ptr + dbr->byte_per_sector * dbr->total_sector;
   memcpy(dbr, img_ptr, sizeof(dbr_t));
 
-  char *data_ptr = img_ptr + 1ll * dbr->byte_per_sector *(dbr->num_of_res_sector + dbr->num_of_fat * dbr->num_of_fat_sector);
+  char *data_ptr = img_ptr + dbr->byte_per_sector *(dbr->num_of_res_sector + dbr->num_of_fat * dbr->num_of_fat_sector);
 
-  sfile_t *root_dir = malloc(sizeof(sfile_t));
-  memcpy(root_dir, data_ptr, sizeof(sfile_t));
-  printf("%s\n", root_dir->name);
-  printf("%x\n", root_dir->m_date);
-  printf("%d\n", root_dir->size);
+  sfile_t *tmp = malloc(sizeof(sfile_t));
+  int cnt_file = 0;
+  for (char *ptr=data_ptr; ptr<end_ptr; ptr+=dbr->byte_per_sector){
+      if (isword(ptr)) {
+        memcpy(tmp, ptr, sizeof(sfile_t));
+        printf("FILE %d: %s\n", cnt_file++, tmp->name);
+      }
+  }
 
   mmap_close(img_ptr);
   return 0;
