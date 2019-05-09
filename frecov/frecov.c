f3d727656a974254fafc20e4696f12bef4f62b7d
tracer-nju <tracer@nju.edu.cn>
2019-05-09 21:18:48 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  21:18:48 up 12:44,  5 users,  load average: 0.84, 0.68, 1.17

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <string.h>
+#include <malloc.h>
 
 int size;
 void *mmap_open(char *name){
@@ -26,15 +28,27 @@ void mmap_close(void *fp){
 }
 
 struct DBR {
-
+  char jmp_intr[3];
+  char version[8];
+  uint16_t char_per_sector;
+  uint8_t cluster_per_sector;
+  uint16_t num_of_res_sector;
+  uint8_t num_of_fat;
+  uint16_t zero_0;
+  uint16_t zero_1;
+  uint8_t material_type;
+  uint16_t zero_2;
+  uint16_t sector_per_tract;
 };
-typedef struct DBR DBR_t;
+typedef struct DBR dbr_t;
 
 #define BYTE(i) (*((uint8_t *) ptr + i))
 #define LOGBYTE(i) printf("%02x\n", BYTE(i));
 int main(int argc, char *argv[]) {
   void *ptr = mmap_open(argv[1]);
- LOGBYTE(0);
+  dbr_t *dbr = malloc(sizeof(sizeof(dbr_t)));
+  memcpy(dbr, ptr, sizeof(dbr_t));
+
   mmap_close(ptr);
   return 0;
 }
