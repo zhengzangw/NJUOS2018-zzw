b7e83e20373fcb75c679bcef58eb309f22504b68
tracer-nju <tracer@nju.edu.cn>
2019-05-09 19:40:48 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  19:40:48 up 11:06,  5 users,  load average: 0.36, 0.20, 0.11

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int main(int argc, char *argv[]) {
+  int fd = open("./filesystem.img", O_RDONLY);
+  assert(fd!=-1);
+
+  struct stat file_stat;
+  fstat(fd, &file_stat);
+  printf("size=%ld", file_stat.st_size);
+
   return 0;
 }
