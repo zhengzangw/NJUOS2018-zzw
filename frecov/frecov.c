1c25c7bd9fc0389a1d35005bcba35fad2cc271fe
tracer-nju <tracer@nju.edu.cn>
2019-05-09 21:05:19 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  21:05:19 up 12:30,  5 users,  load average: 2.45, 2.91, 2.08

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -3,6 +3,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdint.h>
 
 int size;
 void *mmap_open(char *name){
@@ -24,14 +25,16 @@ void mmap_close(void *fp){
   munmap(fp, size);
 }
 
-#define BYTE(i) (*((unsigned char *) ptr + i))
+struct DBR {
+
+};
+typedef struct DBR DBR_t;
+
+#define BYTE(i) (*((uint8_t *) ptr + i))
+#define LOGBYTE(i) printf("%02x\n", BYTE(i));
 int main(int argc, char *argv[]) {
   void *ptr = mmap_open(argv[1]);
-
-  for (int i=0;i<=10;++i){
-      printf("%02x ", BYTE(i));
-  }
-
+ LOGBYTE(0);
   mmap_close(ptr);
   return 0;
 }
