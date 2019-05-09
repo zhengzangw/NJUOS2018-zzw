0a0b7327beb32f983793762a55d20032611b7459
tracer-nju <tracer@nju.edu.cn>
2019-05-09 20:49:44 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  20:49:44 up 12:14,  5 users,  load average: 1.33, 0.55, 0.28

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
   void *ptr = mmap_open(argv[1]);
 
   for (int i=0;i<=10;++i){
-      printf("%04x ", *((unsigned short *)ptr + i));
+      printf("%02x ", *((unsigned char *)ptr + i));
   }
 
   mmap_close(ptr);
