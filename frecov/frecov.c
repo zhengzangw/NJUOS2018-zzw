989aad38ae37ec22b850bc198c59958f17d758d3
tracer-nju <tracer@nju.edu.cn>
2019-05-10 14:20:33 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 35180d95cb0e41718f3a4fd1f2b607da     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  14:20:33 up 1 day,  4:30,  4 users,  load average: 1.79, 0.58, 0.38

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -174,7 +174,7 @@ int main(int argc, char *argv[], char *env[]) {
         bmp_t *bmp_ptr = (bmp_t *)(data_ptr+addr);
         if (!validbmp(bmp_ptr, ptr->size)) continue;
 
-        int status;
+        //int status;
         int fl_in[2], fl_out[2];
         pipe(fl_in); pipe(fl_out);
         int pid = fork();
