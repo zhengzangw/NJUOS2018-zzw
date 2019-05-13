d8438b0fea5b5f4d81c409cde227c0dbe8b873b2
tracer-nju <tracer@nju.edu.cn>
2019-05-13 16:33:36 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f44d1fa910024bf3a1f6e1dfbad4f1ad     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  16:33:36 up  1:25,  1 user,  load average: 0.45, 0.55, 0.40

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,28 +1,5 @@
 #include <kernel.h>
 #include <klib.h>
-#include <devices.h>
-
-sem_t sem_p, sem_c, mutex;
-void producer(void *arg) {
-  device_t *tty = dev_lookup("tty1");
-  while (1) {
-    kmt->sem_wait(&sem_p);
-    kmt->sem_wait(&mutex);
-    tty->ops->write(tty, 0, "I love ", 7);
-    kmt->sem_signal(&mutex);
-    kmt->sem_signal(&sem_c);
-  }
-}
-void customer(void *arg) {
-  device_t *tty = dev_lookup("tty1");
-  while (1) {
-    kmt->sem_wait(&sem_c);
-    kmt->sem_wait(&mutex);
-    tty->ops->write(tty, 0, (char *) arg, strlen((char *) arg));
-    kmt->sem_signal(&mutex);
-    kmt->sem_signal(&sem_p);
-  }
-}
 
 int main() {
   _ioe_init();
@@ -30,29 +7,6 @@ int main() {
 
   // call sequential init code
   os->init();
-
-  kmt->sem_init(&sem_p, "producer-sem", 1);
-  kmt->sem_init(&sem_c, "customer-sem", 0);
-  kmt->sem_init(&mutex, "mutex", 1);
-
-  kmt->create(pmm->alloc(sizeof(task_t)), "p-task", producer, NULL);
-
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "you\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "him\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "her\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "they\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "us\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "it\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "the god\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "american\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "europian\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "japanese\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "russian\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "indian\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "boy next door\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "fucking coming\n");
-  kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, "jyy and oslabs (X)\n");
-
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
