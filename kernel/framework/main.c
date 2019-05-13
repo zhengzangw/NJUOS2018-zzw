2ffaf31530fb8ce9901a8043e9a5a4c1890ab0ab
tracer-nju <tracer@nju.edu.cn>
2019-05-13 16:31:31 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f44d1fa910024bf3a1f6e1dfbad4f1ad     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  16:31:31 up  1:23,  1 user,  load average: 0.95, 0.60, 0.39

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,22 +1,5 @@
 #include <kernel.h>
 #include <klib.h>
-#include <common.h>
-
-sem_t empty, fill;
-void producer(){
-  while (1) {
-    kmt->sem_wait(&empty);
-    lprintf("(");
-    kmt->sem_signal(&fill);
-  }
-}
-void consumer(){
-  while (1){
-    kmt->sem_wait(&fill);
-    lprintf(")");
-    kmt->sem_signal(&empty);
-  }
-}
 
 int main() {
   _ioe_init();
@@ -24,11 +7,6 @@ int main() {
 
   // call sequential init code
   os->init();
-  kmt->sem_init(&empty, "empty", 10);
-  kmt->sem_init(&fill, "fill", 0);
-  kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
-
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
