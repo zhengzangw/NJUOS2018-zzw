8cead1e1fee25d0de6dc4ad5354bcdae38c85bf9
tracer-nju <tracer@nju.edu.cn>
2019-05-13 15:59:20 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f44d1fa910024bf3a1f6e1dfbad4f1ad     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  15:59:20 up 51 min,  1 user,  load average: 0.01, 0.04, 0.07

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -43,12 +43,13 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
     kmt->spin_lock(&lock_kmt);
     for (int i = 0; i < MAXTASK; ++i) {
         task_t *nxt = tasks[(seed + i + 1) % MAXTASK];
-        int t = nxt==NULL;
-        if (!t) Log("%d %d %d",t, i,nxt==NULL);
+#pragma optimize("", off)
+        if (nxt) Log("%d %d", i,nxt==NULL);
         if (nxt && nxt->run == 0 && nxt->sleep == 0) {
             ret = nxt;
             break;
         }
+#pragma optimize("", on)
     }
     if (ret == NULL) {
         ret = &cpudefaulttask[_cpu()];
