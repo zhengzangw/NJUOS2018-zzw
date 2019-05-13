4878fa32c008409b5a237cc3dac9967dcae9c80a
tracer-nju <tracer@nju.edu.cn>
2019-05-13 11:37:29 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 8ccf08340a914ff78e95554035c1bac5     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  11:37:29 up 32 min,  5 users,  load average: 0.93, 0.43, 0.33

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,11 +1,26 @@
 #include <kernel.h>
 #include <klib.h>
-#include <common.h>
 
-void non(){
+void logging(void *arg) {
   while (1){
-    _yield();
+    lprintf("%s", (char *)arg);
+    assertIF1();
   }
+}
+
+char* str[] = {"1", "2", "3", "4"};
+void test(void *arg){
+    while (1){
+        task_t *tmp=NULL;
+        for (int i=0;i<=3;++i){
+            tmp = pmm->alloc(sizeof(task_t));
+            if (kmt->create(tmp, "auto-gem", logging, str[i])){
+                pmm->free(tmp);
+                tmp = NULL;
+            }
+        }
+        if (tmp) kmt->teardown(tmp);
+    }
 }
 
 int main() {
@@ -14,10 +29,7 @@ int main() {
 
   // call sequential init code
   os->init();
-  for (int i=0;i<10;++i){
-	kmt->create(pmm->alloc(sizeof(task_t)), "non", non, NULL);
-  }
-
+  kmt->create(pmm->alloc(sizeof(task_t)), "first", test, NULL);
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
