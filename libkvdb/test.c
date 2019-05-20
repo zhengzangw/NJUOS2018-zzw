75d87f23b5bb358d910a216f99fa746e7f8749bc
tracer-nju <tracer@nju.edu.cn>
2019-05-21 01:09:20 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: de47762edd07463d9fc13cc7c91ca244     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  01:09:20 up 10:57,  5 users,  load average: 0.02, 0.04, 0.00

---
+++ libkvdb/test.c
@@ -0,0 +1,16 @@
+#include "kvdb.h"
+#include <stdlib.h>
+
+int main(){
+    kvdb_t db;
+    const char *key = "operating-systems";
+    char *value;
+
+    kvdb_open(&db, "a.db");
+    //kvdb_put(&db, key, "three-easy-pieces");
+    //value = kvdb_get(&db, key);
+    kvdb_close(&db);
+    //printf("[%s]: [%s]\n", key, value);
+    //free(value);
+    return 0;
+}
