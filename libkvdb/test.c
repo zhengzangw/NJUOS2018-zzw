25cbca7814f923cfabc376f0dfaba0b3109e003c
tracer-nju <tracer@nju.edu.cn>
2019-05-21 22:16:41 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: de47762edd07463d9fc13cc7c91ca244     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  22:16:41 up 1 day,  4:27,  5 users,  load average: 0.53, 0.56, 0.52

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -6,22 +6,17 @@
     if (value!=NULL) printf("[%s]: [%s]\n", key, value);\
     free(value)
 
-int main(){
+int main(int argc, char *argv[]){
     kvdb_t db;
     char *value;
 
     kvdb_open(&db, "a.db");
-    kvdb_put(&db, "zzw", "171860658");
-    kvdb_put(&db, "jyy", "ree-easy-pieces");
-    kvdb_put(&db, "wrong", "e-easy-pieces");
 
     while (1){
-    //for (int i=0;i<100;++i){
-    kvdb_get_log(&db, "zzw");
-    kvdb_get_log(&db, "wrong");
-    kvdb_get_log(&db, "jyy");
-    kvdb_get_log(&db, "problem");
-    kvdb_get_log(&db, "zzw");
+      kvdb_put(&db, "zzw", argv[1]);
+      kvdb_put(&db, "jyy", "ree-easy-pieces");
+      kvdb_put(&db, "wrong", "e-easy-pieces");
+      kvdb_get_log(&db, "zzw");
     }
 
     kvdb_close(&db);
