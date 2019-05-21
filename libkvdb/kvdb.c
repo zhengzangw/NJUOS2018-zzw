2b9db7bb41bdf5766c43a30d5222df390524373b
tracer-nju <tracer@nju.edu.cn>
2019-05-21 20:29:40 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: de47762edd07463d9fc13cc7c91ca244     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  20:29:40 up 1 day,  2:40,  5 users,  load average: 0.12, 0.15, 0.19

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -13,17 +13,67 @@ int kvdb_open(kvdb_t *db, const char *filename){
         }
     }
     db->file = fopen(filename, "w+");
-    return -1;
+    return 0;
 }
+
 int kvdb_close(kvdb_t *db){
     fclose(db->file);
-    return -1;
+    return 0;
 }
+
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
     fseek(db->file, 0, SEEK_END);
     fprintf(db->file, "\n%s %s\n", key, value);
-    return -1;
+    return 0;
 }
+
+#define backspace(file) \
+    fseek(file, -1, SEEK_CUR)
+#define fscanf_bak(file, c) \
+    backspace(file); \
+    fscanf(file, "%c", &c);\
+    backspace(file)
+#define fscanf_bak_0a(file, c) \
+    fscanf_bak(file, c); \
+    assert(flag=='\n');
+
 char *kvdb_get(kvdb_t *db, const char *key){
+    char flag;
+    int finded = 0, len;
+    fseek(db->file, 0, SEEK_END);
+
+    while (!finded){
+        fscanf_bak_0a(db->file, flag);
+
+        len = 0;
+        flag = '\0';
+        while (flag!=' '){
+            len++;
+            fscanf_bak(db->file, flag);
+        }
+        printf("len=%d\n", len);
+        assert(0);
+        /*
+        fseek(db->file, 2, SEEK_CUR);
+        char* value = malloc(len);
+        fscanf(db->file, "%s", value);
+        fseek(db->file, -len, SEEK_CUR);
+
+        fseek(db->file, -1, SEEK_CUR);
+        fscanf_cb(db->file, flag);
+        assert(flag=='\n');
+        int len = 0;
+        flag = '\0';
+        while (flag!='\n'){
+            len++;
+            fscanf_cb(db->file, flag);
+        }
+        fseek(db->file, 2, SEEK_CUR);
+
+        fseek(db->file, -1, SEEK_CUR);
+        fscanf_cb(db->file, flag);
+        assert(flag=='\n');
+        */
+    }
     return NULL;
 }
--- libkvdb/test.c
+++ libkvdb/test.c
@@ -4,15 +4,15 @@
 int main(){
     kvdb_t db;
     const char *key = "operating-systems";
-    //char *value;
+    char *value;
 
     kvdb_open(&db, "a.db");
     kvdb_put(&db, key, "three-easy-pieces");
-    kvdb_put(&db, key, "three-easy-pieces");
-    kvdb_put(&db, key, "three-easy-pieces");
-    //value = kvdb_get(&db, key);
+    kvdb_put(&db, key, "ree-easy-pieces");
+    kvdb_put(&db, key, "e-easy-pieces");
+    value = kvdb_get(&db, key);
     kvdb_close(&db);
-    //printf("[%s]: [%s]\n", key, value);
-    //free(value);
+    printf("[%s]: [%s]\n", key, value);
+    free(value);
     return 0;
 }
