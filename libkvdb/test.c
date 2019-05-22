6dc15ebc136bbd9112614f49036afbd7ae7ac36b
tracer-nju <tracer@nju.edu.cn>
2019-05-22 11:19:15 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: cf3d305de82446fe9e74973dd9dd0000     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  11:19:15 up  6:24,  5 users,  load average: 0.46, 0.41, 0.39

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -2,25 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <pthread.h>
 
 #define kvdb_get_log(db, key)\
     value = kvdb_get(db, key);\
     if (value!=NULL) printf("[%s]: [%s]\n", key, value);\
     free(value)
 
-int main(int argc, char *argv[]){
-    kvdb_t db;
-    char *value;
-
-    kvdb_open(&db, "a.db");
-    kvdb_put(&db, "zzw", "awsl");
-
-    kvdb_get_log(&db, "zzw");
+kvdb_t db;
+pthread_t threads[10];
+typedef struct ds {
+    char *key, *value;
+} ds_t;
+void *producer(void* args){
+    ds_t *data = (ds_t *)args;
     while (1){
-      kvdb_put(&db, "zzw", argv[1]);
-      kvdb_get_log(&db, "zzw");
-      assert(value!=NULL);
+        kvdb_put(&db, data->key, data->value);
     }
+}
+
+void *consumer(void* key){
+    char *kkey = (char *)key;
+    char *value;
+    while (1){
+      kvdb_get_log(&db, kkey);
+    }
+}
+
+int main(int argc, char *argv[]){
+    char *value;
+    kvdb_open(&db, "a.db");
+
+    ds_t tmp;
+    tmp.key = "zzw"; tmp.value = "171860658";
+    pthread_create(&threads[0], NULL, producer, (void*)&tmp);
+
+    pthread_create(&threads[5], NULL, consumer, (void*)"zzw");
+
+    void *tptr;
+    pthread_join(threads[0], &tptr);
+    pthread_join(threads[5], &tptr);
 
     kvdb_close(&db);
     return 0;
