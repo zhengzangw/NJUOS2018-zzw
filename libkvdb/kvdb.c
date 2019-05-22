b1fb0734affcf0d23149e3e9cfb0aecd596b56b2
tracer-nju <tracer@nju.edu.cn>
2019-05-22 10:14:37 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: cf3d305de82446fe9e74973dd9dd0000     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  10:14:37 up  5:19,  5 users,  load average: 0.02, 0.21, 0.32

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <sys/file.h>
+#include <pthread.h>
 
 #define check_file_lock(ret,fail) do { \
     if (ret==-1) { \
@@ -14,7 +15,6 @@
     } } while (0)
 
 #define file_lock_ex(db,fail) \
-    ret = flock(fileno(db->file), LOCK_EX);\
     check_file_lock(ret,fail)
 #define file_lock_sh(db,fail) \
     ret = flock(fileno(db->file), LOCK_SH);\
@@ -22,6 +22,49 @@
 #define file_lock_un(db,fail) \
     ret = flock(fileno(db->file), LOCK_UN);\
     check_file_lock(ret,fail)
+
+#define assert(exp)\
+    do {if (!(exp)){\
+        error = 1;\
+        break;\
+    }}while (0)
+#define backspace(file, num) \
+    fseek(file, num, SEEK_CUR)
+#define fscanf_bak(file, c) \
+    backspace(file, -1);\
+    fscanf(file, "%c", &c);\
+    backspace(file, -1);
+#define fscanf_bak_0a(file, c) \
+    fscanf_bak(file, c); \
+    assert(c=='\n')
+#define fscanf_val(file, des, len)\
+    des = malloc(len);\
+    fscanf(file, "%s", des);\
+    fseek(file, -len-1, SEEK_CUR)
+
+static inline bool ishead(kvdb_t *db){
+    return ftell(db->file)==0;
+}
+
+int check_journal(kvdb_t *db){
+    char flag;
+    bool consist = 0, has_blank = 0;
+    int len;
+
+    fseek(db->file, -1, SEEK_END);
+    while (!consist && !ishead(db)){
+        fscanf_bak(db->file, flag);
+        if (flag=='\n'){
+            has_blank = 0;
+            flag = '\0';
+            while (flag!='\n'){
+                fscanf_bak(db->file, flag);
+                if (flag == ' ') has_blank = 1;
+            }
+            if (flag == '\n' && has_blank == 1) consist = 1;
+        }
+    }
+}
 
 int ret;
 int kvdb_open(kvdb_t *db, const char *filename){
@@ -39,6 +82,11 @@ int kvdb_open(kvdb_t *db, const char *filename){
         fclose(tmpf);
     }
 
+    if (pthread_mutex_init(&db->mutex, NULL) != 0){
+        perror("Cannot create lock");
+        return -1;
+    }
+
     db->file = fopen(filename, "r+");
     if (db->file == NULL){
         perror("Cannot open file");
@@ -49,42 +97,22 @@ int kvdb_open(kvdb_t *db, const char *filename){
 }
 
 int kvdb_close(kvdb_t *db){
+    pthread_mutex_destroy(&db->mutex);
+    if (db->file==NULL) return -1;
     fclose(db->file);
     return 0;
 }
 
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
-    file_lock_ex(db, -1);
+    if (db->file==NULL) return -1;
+    if (pthread_mutex_lock(&db->mutex)!=0) return -1;
+    if (flock(fileno(db->file), LOCK_EX) !=0) return -1;
     fseek(db->file, 0, SEEK_END);
     fprintf(db->file, "\n%s %s\n", key, value);
     fsync(fileno(db->file));
-    file_lock_un(db, -1);
+    if (flock(fileno(db->file), LOCK_UN) !=0) return -1;
+    if (pthread_mutex_unlock(&db->mutex)) return -1;
     return 0;
-}
-
-//#include <assert.h>
-#define assert(exp)\
-    do {if (!(exp)){\
-        error = 1;\
-        break;\
-    }}while (0)
-#define backspace(file, num) \
-    fseek(file, num, SEEK_CUR)
-#define fscanf_bak(file, c) \
-    fscanf(file, "%c", &c);\
-    backspace(file, -1);\
-    backspace(file, -1)
-#define fscanf_bak_0a(file, c) \
-    fscanf_bak(file, c); \
-    assert(c=='\n')
-#define fscanf_val(file, des, len)\
-    des = malloc(len);\
-    fseek(file, 1, SEEK_CUR);\
-    fscanf(file, " %s", des);\
-    fseek(file, -len-1, SEEK_CUR)
-
-static inline bool ishead(kvdb_t *db){
-    return ftell(db->file)==0;
 }
 
 char *kvdb_get(kvdb_t *db, const char *key){
@@ -93,11 +121,11 @@ char *kvdb_get(kvdb_t *db, const char *key){
     bool finded = 0, error = 0;
     int len;
 
-    file_lock_sh(db, NULL);
-    fseek(db->file, -1, SEEK_END);
-    int cnt = 0;
+    if (db->file==NULL) return NULL;
+    if (pthread_mutex_lock(&db->mutex)!=0) return NULL;
+    if (flock(fileno(db->file), LOCK_SH) !=0) return NULL;
+    fseek(db->file, 0, SEEK_END);
     while (!finded && !ishead(db)){
-        cnt ++;
         //Check the last \n
         fscanf_bak_0a(db->file, flag);
         //Value
@@ -105,10 +133,13 @@ char *kvdb_get(kvdb_t *db, const char *key){
         while (flag!=' ' && !ishead(db)){
             len++;
             fscanf_bak(db->file, flag);
-            assert(flag!='\n');
         }
         assert(flag==' ');
         fscanf_val(db->file, tmp_value, len);
+        printf("%s\n", tmp_value);
+            fscanf_bak(db->file, flag);
+            printf("c=%c\n",flag);
+        return NULL;
         //Key
         len = 0; flag = '\0';
         while (flag!='\n' && !ishead(db)){
@@ -132,7 +163,8 @@ char *kvdb_get(kvdb_t *db, const char *key){
         }
         free(tmp_key);
     }
-    file_lock_un(db, NULL);
+    if (flock(fileno(db->file), LOCK_UN) !=0) return NULL;
+    pthread_mutex_unlock(&db->mutex);
 
     if (finded){
         return tmp_value;
--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -2,9 +2,11 @@
 #define __KVDB_H__
 
 #include <stdio.h>
+#include <pthread.h>
 
 struct kvdb {
   FILE* file;
+  pthread_mutex_t mutex;
 };
 typedef struct kvdb kvdb_t;
 
