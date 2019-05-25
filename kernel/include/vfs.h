ee279ba95a266f0bc00192fc1324ecea8a05e121
tracer-nju <tracer@nju.edu.cn>
2019-05-25 19:49:57 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 5ef0e55f4dd44562919540c91509a25b     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  19:49:57 up 16:29,  5 users,  load average: 1.18, 0.31, 0.14

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -3,6 +3,42 @@
 
 #include <common.h>
 
+typedef struct file {
+  int refcnt;
+  inode_t *inode;
+  uint64_t offset;
+} file_t;
+
+typedef struct inodeops {
+  int (*open)(file_t *file, int flags);
+  int (*close)(file_t *file);
+  ssize_t (*read)(file_t *file, char *buf, size_t size);
+  ssize_t (*write)(file_t *file, const char *buf, size_t size);
+  off_t (*lseek)(file_t *file, off_t offset, int whence);
+  int (*mkdir)(const char *name);
+  int (*rmdir)(const char *name);
+  int (*link)(const char *name, inode_t *inode);
+  int (*unlink)(const char *name);
+} inodeops_t;
+
+typedef struct inode {
+  int refcnt;
+  void *ptr;
+  filesystem_t *fs;
+  inodeops_t *ops;
+} inode_t;
+
+typedef struct fsops {
+  void (*init)(filesystem_t *fs, const char *name, device_t *dev);
+  inode_t *(*lookup)(filesystem_t *fs, const char *name, int flags);
+  int (*close)(inode_t *inode);
+} fsops_t;
+
+typedef struct filesystem {
+  fsops_t *ops;
+  device_t *dev;
+} filesystem_t;
+
 typedef struct {
   void (*init)();
   int (*access)(const char *path, int mode);
