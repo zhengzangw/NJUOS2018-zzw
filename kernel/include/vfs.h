cf6d383a281b5bfeff23da8718189c0456679789
tracer-nju <tracer@nju.edu.cn>
2019-05-25 19:20:08 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 5ef0e55f4dd44562919540c91509a25b     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  19:20:08 up 15:59,  5 users,  load average: 0.78, 0.36, 0.26

---
+++ kernel/include/vfs.h
@@ -0,0 +1,22 @@
+#ifndef __VFS_H__
+#define __VFS_H__
+
+#include <common.h>
+
+typedef struct {
+  void (*init)();
+  int (*access)(const char *path, int mode);
+  int (*mount)(const char *path, filesystem_t *fs);
+  int (*unmount)(const char *path);
+  int (*mkdir)(const char *path);
+  int (*rmdir)(const char *path);
+  int (*link)(const char *oldpath, const char *newpath);
+  int (*unlink)(const char *path);
+  int (*open)(const char *path, int flags);
+  ssize_t (*read)(int fd, void *buf, size_t nbyte);
+  ssize_t (*write)(int fd, void *buf, size_t nbyte);
+  off_t (*lseek)(int fd, off_t offset, int whence);
+  int (*close)(int fd);
+} MODULE(vfs);
+
+#endif
---
+++ kernel/src/vfs.c
@@ -0,0 +1,70 @@
+#include <common.h>
+#include <vfs.h>
+
+void vfs_init(){
+
+}
+
+int vfs_access(const char *path, int mode){
+    return 0;
+}
+
+int vfs_mount(const char *path, filesystem_t *fs){
+    return 0;
+}
+
+int vfs_unmount(const char *path){
+    return 0;
+}
+
+int vfs_mkdir(const char *path){
+    return 0;
+}
+
+int vfs_rmdir(const char *path){
+    return 0;
+}
+
+int vfs_link(const char *oldpath, const char *newpath){
+    return 0;
+}
+
+int vfs_unlink(const char *path){
+    return 0;
+}
+
+int vfs_open(const char *path, int flags){
+    return 0;
+}
+
+ssize_t vfs_read(int fd, void *buf, size_t nbyte){
+    return 0;
+}
+
+ssize_t vfs_write(int fd, void *buf, size_t nbyte){
+    return 0;
+}
+
+off_t vfs_lseek(int fd, off_t offset, int whence){
+    return 0;
+}
+
+int vfs_close(int fd){
+    return 0;
+}
+
+MODULE_DEF(vfs){
+    .init = vfs_init,
+    .access = vfs_access,
+    .mount = vfs_mount,
+    .unmount = vfs_unmount,
+    .mkdir = vfs_mkdir,
+    .rmdir = vfs_rmdir,
+    .link = vfs_link,
+    .unlink = vfs_unlink,
+    .open = vfs_open,
+    .read = vfs_read,
+    .write = vfs_write,
+    .lseek = vfs_lseek,
+    .close = vfs_close,
+};
