2dc6abc1ed5027c93c74683bf936e1a5d59ca74c
tracer-nju <tracer@nju.edu.cn>
2019-05-27 21:20:23 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 0d004d2a24a84499a43b4023a56a8a77     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  21:20:23 up 23:59,  5 users,  load average: 0.22, 0.20, 0.18

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -35,7 +35,7 @@ int get_free_data(filesystem_t *fs, device_t *dev){
 
 void ext2_init(filesystem_t *fs, const char *name, device_t *dev){
     printf("size=%ld", sizeof(ext2_inode));
-    ext2_inode_t *root = pmm->malloc(sizeof(ext2_inode));
+    ext2_inode_t *root = pmm->malloc(sizeof(ext2_inode_t));
     memset(root, 0, sizeof(root));
     root->exists = 1;
     root->type = DR;
