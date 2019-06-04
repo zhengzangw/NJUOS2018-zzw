bd10cde80200d1e2ba2ac84b05f6598584391929
tracer-nju <tracer@nju.edu.cn>
2019-06-04 20:36:45 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f0d9ba2eefc848428106467141ea5c85     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  20:36:45 up  3:57,  5 users,  load average: 0.29, 0.36, 0.42

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -106,7 +106,7 @@ void ext2_init(filesystem_t *fs, const char *name, device_t *dev){
     ext2_inode_t *root = (ext2_inode_t *)(pmm->alloc(sizeof(ext2_inode_t)));
     int x = IMAP;
     void *logs = pmm->alloc(BLOCK_BYTES);
-    //dev->ops->read(dev, BLOCK(x), &logs, BLOCK_BYTES);
+    dev->ops->read(dev, BLOCK(x), logs, BLOCK_BYTES);
     printf("======== LOG BLOCK =======\n");
     for (int i=0;i<BLOCK_BYTES;++i){
         printf("%02x ", *((char *)logs+i));
