68901463b4ce6eac5a1b937bb151ca989ab6e20b
tracer-nju <tracer@nju.edu.cn>
2019-06-04 21:13:47 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f0d9ba2eefc848428106467141ea5c85     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  21:13:47 up  4:34,  5 users,  load average: 0.02, 0.13, 0.27

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -16,13 +16,19 @@ void bzero(int x, device_t* dev){
 
 void LogBlock(int x, device_t* dev) {
     void *logs = pmm->alloc(BLOCK_BYTES);
-    dev->ops->read(dev, BLOCK(x), logs, BLOCK_BYTES);
-    printf("======== LOG BLOCK %d =======\n", x);
-    for (int i=0;i<BLOCK_BYTES;++i){
+    printf("======== log block %d =======\n", x);
+    for (int i=0;i<block_bytes;++i){
         printf("%02x ", *((unsigned char *)logs+i));
         if ((i+1)%(1<<6)==0) printf("\n");
     }
-    printf("======== LOG ENDED %d =======\n", x);
+    printf("======== log ended %d =======\n", x);
+    dev->ops->read(dev, BLOCK(x), logs, BLOCK_BYTES);
+    printf("======== log block %d =======\n", x);
+    for (int i=0;i<block_bytes;++i){
+        printf("%02x ", *((unsigned char *)logs+i));
+        if ((i+1)%(1<<6)==0) printf("\n");
+    }
+    printf("======== log ended %d =======\n", x);
     pmm->free(logs);
 }
 
@@ -127,8 +133,8 @@ void ext2_init(filesystem_t *fs, const char *name, device_t *dev){
     pmm->free(root);
 
     LogBlock(IMAP, dev);
-    LogBlock(DMAP, dev);
-    LogBlock(ITABLE, dev);
+    //LogBlock(DMAP, dev);
+    //LogBlock(ITABLE, dev);
 }
 
 inode_t* ext2_lookup(filesystem_t *fs, const char *name, int flags){
