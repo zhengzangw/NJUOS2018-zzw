ff749a23944f201fb4af325c9fc9930743627ade
tracer-nju <tracer@nju.edu.cn>
2019-06-05 16:27:25 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f0d9ba2eefc848428106467141ea5c85     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  16:27:25 up 11:53,  3 users,  load average: 0.64, 0.35, 0.24

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -266,21 +266,20 @@ void ext2_create_dir(device_t *dev, const char *name, int isroot){
 inode_t* ext2_lookup(filesystem_t *fs, const char *name, int flags);
 void ext2_init(filesystem_t *fs, const char *name, device_t *dev){
     Log("EXT2 INFO: inode size=%ld", sizeof(ext2_inode_t));
-
+    Log("%p", fs->dev);
     //clear
     bzero(IMAP);
+    assert(dev == fs->dev);
     bzero(DMAP);
     for (int i=ITABLE; i<ITABLE+ITABLE_NUM; ++i){
         bzero(i);
     }
 
-    assert(dev == fs->dev);
     ext2_create_dir(dev, name, 1);
     ext2_create_dir(dev, "/bin", 0);
     ext2_create_dir(dev, "/test", 0);
     ext2_create_dir(dev, "/bin/a.txt", 0);
 
-    assert(dev == fs->dev);
     inode_t* tmp = ext2_lookup(fs, "/bin/a.txt", 0);
     Log("inode=%d", ((ext2_inode_t*)tmp->fs_inode)->index);
     pmm->free(tmp);
