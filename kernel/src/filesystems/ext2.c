22172b9d149d39d59e9b106c71161baf490ccb4e
tracer-nju <tracer@nju.edu.cn>
2019-06-05 15:57:55 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f0d9ba2eefc848428106467141ea5c85     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  15:57:55 up 11:24,  3 users,  load average: 0.34, 0.37, 0.34

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -130,6 +130,7 @@ ext2_inode_t* ext2_create_inode(device_t *dev, uint8_t type, uint8_t per){
 
 int ext2_dir_search(device_t *, ext2_inode_t*, const char*);
 ext2_inode_t* ext2_lookup_dir(device_t *dev, const char *name){
+    Log("!");
     ext2_inode_t *inode = (ext2_inode_t *)(pmm->alloc(sizeof(ext2_inode_t)));
     dev->ops->read(dev, TABLE(0), inode, INODE_BYTES);
 
@@ -137,7 +138,6 @@ ext2_inode_t* ext2_lookup_dir(device_t *dev, const char *name){
     char *pre = NULL, *post = NULL, *tmp;
     tmp = pmm->alloc(strlen(name)+1);
     strcpy(tmp, name);
-    Log("!");
     int splited = split(tmp, &pre, &post);
     Log("tmp=%s name=%s splited=%d", tmp, name, splited);
     while (splited){
