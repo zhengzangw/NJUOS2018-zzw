f39b67d0e0ab82c052b2c07639a044a8b6354c0b
tracer-nju <tracer@nju.edu.cn>
2019-06-05 11:29:12 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: f0d9ba2eefc848428106467141ea5c85     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  11:29:12 up  7:55,  3 users,  load average: 0.54, 0.52, 0.32

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -217,8 +217,9 @@ void ext2_create_dir(device_t *dev, const char *name, int isroot){
         char *pre, *post;
         split2(name, &pre, &post);
         Log("pre=%s post=%s", pre, post);
-    assert(0);
         ext2_inode_t* father = ext2_lookup_dir(dev, pre);
+        Log("fat=%d", father->inode);
+        assert(0);
         ext2_inode_t* dir = ext2_create_inode(dev, DR, per);
         ext2_create_entry(dev, father, dir, post, DR);
         ext2_create_entry(dev, dir, dir, ".", DR);
