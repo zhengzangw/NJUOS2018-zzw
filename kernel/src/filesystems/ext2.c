343908e51e3f58f74b33ea4f42e0501dabab8603
tracer-nju <tracer@nju.edu.cn>
2019-06-09 21:36:09 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  21:36:09 up  7:50,  9 users,  load average: 0.93, 0.72, 0.70

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -175,7 +175,7 @@ typedef struct dir_entry dir_entry_t;
 void ext2_create_entry(device_t *dev, ext2_inode_t* inode, ext2_inode_t* entry_inode, const char* entry_name, uint32_t type){
     dir_entry_t* dir = balloc(sizeof(dir_entry_t));
     dir->inode = entry_inode->index;
-    Logint(dir->inode);
+    Log("name = %s, inode = %d", entry_name, entry_inode);
     uint32_t name_len = strlen(entry_name);
     dir->name_len = name_len+1;
     dir->rec_len = sizeof(dir_entry_t)+dir->name_len;
