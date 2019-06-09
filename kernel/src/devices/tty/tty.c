7f79940b21210aa77c68bc1510654c8a085a6bc1
tracer-nju <tracer@nju.edu.cn>
2019-06-09 20:15:27 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  20:15:27 up  6:29,  9 users,  load average: 3.00, 2.36, 1.38

--- kernel/src/devices/tty/tty.c
+++ kernel/src/devices/tty/tty.c
@@ -286,6 +286,7 @@ void echo_task(void *name){
         sprintf(file, "%s%s", pwd, tmpname);\
         Log("file = %s", file);\
     }
+#define SUCCESS "[\e[#m32Success \e[#m0]"
 
 void shell_task(void *name){
     char *pwd = "/";
@@ -323,6 +324,7 @@ void shell_task(void *name){
         } else if (strncmp(line, "mkdir", 5)==0){
             getname(5);
             vfs->mkdir(file);
+            sprintf(text, SUCCESS "create %s", file);
         } else {
             text[0] = '\0';
         }
