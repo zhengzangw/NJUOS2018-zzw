2b875020f9dd946d21d9356fa78aca70c0f76f1b
tracer-nju <tracer@nju.edu.cn>
2019-06-10 00:32:23 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  00:32:23 up 10:15,  9 users,  load average: 1.08, 0.76, 0.73

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -260,7 +260,7 @@ int ext2_create_dir(device_t *dev, const char *name, int isroot){
 /*======== API ============*/
 inode_t* ext2_lookup(filesystem_t *fs, const char *name, int flags);
 void ext2_init(filesystem_t *fs, const char *name, device_t *dev){
-    printf("EXT2 INFO:\n Block Size:%#lx\n Inode Nums:%d\nInode Start:%d\n Inode Size:%#lx\n Data Start:%d\n",BLOCK_BYTES, ITABLE_NUM, ITABLE, sizeof(ext2_inode_t), DATA_B);
+    printf("==== EXT2 INFO ====\n Block Size:%#lx\n Inode Nums:%d\nInode Start:%d\n Inode Size:%#lx\n Data Start:%d\n",BLOCK_BYTES, ITABLE_NUM, ITABLE, sizeof(ext2_inode_t), DATA_B);
     //clear
     bzero(dev, IMAP);
     bzero(dev, DMAP);
