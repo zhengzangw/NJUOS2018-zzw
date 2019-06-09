a9b286ec31bfcf0cf3191a920bda62b024647fbf
tracer-nju <tracer@nju.edu.cn>
2019-06-09 18:43:45 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  18:43:45 up  4:57,  9 users,  load average: 1.46, 0.58, 0.33

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -280,7 +280,7 @@ int ext2_inode_close(file_t *file){
 ssize_t ext2_inode_read(file_t *file, char *buf, size_t size){
             dir_entry_t* cur = pmm->alloc(sizeof(dir_entry_t));
     ext2_inode_t* inode = file->inode->fs_inode;
-    device_t* dev = file->fs->dev;
+    device_t* dev = file->inode->fs->dev;
             int offset = 0;
             int cnt = 0 ,ret = 0, buf_offset = 0;
     switch (file->inode->type){
