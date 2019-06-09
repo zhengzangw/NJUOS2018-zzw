fed0721b7718fab22057b6afd5c253c68a03ac1b
tracer-nju <tracer@nju.edu.cn>
2019-06-09 23:49:14 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  23:49:14 up  9:32,  9 users,  load average: 0.38, 0.76, 0.79

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -167,6 +167,7 @@ int vfs_close(int fd){
 
 ssize_t vfs_read(int fd, void *buf, size_t nbyte){
     inode_t* tmp = cputask[_cpu()]->flides[fd]->inode;
+    Logint(tmp->ops);
     assert(0);
     return tmp->ops->read(cputask[_cpu()]->flides[fd], (char *)buf, nbyte);
 }
