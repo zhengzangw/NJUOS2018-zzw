e7d70b10ae22a3e5c7ee636c68d9ed9e0737f844
tracer-nju <tracer@nju.edu.cn>
2019-06-10 13:25:26 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: a892a2ec8a41422b8b338a86230a6cbb     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  13:25:26 up 14:05,  1 user,  load average: 0.57, 0.48, 0.59

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -36,7 +36,6 @@ void LogBlock(device_t* dev, int x) {
 int read_map(device_t *dev, int block, uint8_t i){
     uint8_t m = 1<<(i%8), b;
     dev->ops->read(dev, MAP(block,i), &b, sizeof(uint8_t));
-    Logint(m);
     Logint(b);
     return ((b&m)!=0);
 }
@@ -45,14 +44,10 @@ int write_map(device_t* dev, int block, int i, uint8_t x){
     assert(x==0||x==1);
     uint8_t m = 1<<(i%8), b;
     dev->ops->read(dev, MAP(block, i), &b, sizeof(uint8_t));
+    Logint(b);
     assert(read_map(dev, block, i)!=x);
-    Logint(b);
-    Logint(m);
     if (x==1) b |= m; else b &= ~m;
-    Logint(b);
     dev->ops->write(dev, MAP(block, i), &b, sizeof(uint8_t));
-    Logint(x);
-    Logint(read_map(dev,block,i));
     assert(read_map(dev, block, i)==x);
     return 0;
 }
