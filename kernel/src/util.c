f8f54352a4014c40402560c21f15be421c695453
tracer-nju <tracer@nju.edu.cn>
2019-06-11 16:27:17 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 4f79b25380b94bc29c75d98a203ea4ae     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  16:27:17 up  6:44,  1 user,  load average: 0.59, 0.75, 0.73

--- kernel/src/util.c
+++ kernel/src/util.c
@@ -38,7 +38,7 @@ char *filename(const char* path){
             break;
         }
     }
-    Log("path = %s, ret = %s", path, ret);
+    //Log("path = %s, ret = %s", path, ret);
     return ret;
 }
 
@@ -54,7 +54,7 @@ char *alldir(const char *path){
             break;
         }
     }
-    Log("path = %s, ret = %s", path, ret);
+    //Log("path = %s, ret = %s", path, ret);
     return ret;
 }
 
@@ -71,40 +71,5 @@ char *postname(const char *path){
         }
     }
     //Log("path = %s, ret = %s", path, ret);
-    return ret;
-}
-
-// 1 if splited, 0 if done
-// /bin/test/a -> / + bin/test/a
-int split(const char *path, char **pre, char **post){
-    int ret = 0, len = strlen(path);
-    for (int i=0;i<len;++i){
-        if (path[i]=='/'){
-            ret = 1;
-            *pre = balloc(i+2);
-            strncpy(*pre, path, i+1);
-            (*pre)[i+1] = '\0';
-            *post = balloc(len-i+2);
-            strncpy(*post, path+i+1, len-i+1);
-            break;
-        }
-    }
-    if (strlen(*post)==0) ret = 0;
-    return ret;
-}
-// /bin/test/a -> / + /bin/test/
-int split2(const char *path, char **pre, char **post){
-    int ret = 0, len = strlen(path);
-    for (int i=len-1;i>=0;--i){
-        if (path[i]=='/'){
-            ret = 1;
-            *pre = pmm->alloc(i+2);
-            strncpy(*pre, path, i+1);
-            (*pre)[i+1] = '\0';
-            *post = pmm->alloc(len-i+2);
-            strncpy(*post, path+i+1, len-i+1);
-            break;
-        }
-    }
     return ret;
 }
