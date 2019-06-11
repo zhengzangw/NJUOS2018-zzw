c613f6e09fc08c0132a6ef2072b776a925495d92
tracer-nju <tracer@nju.edu.cn>
2019-06-11 14:13:14 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 4f79b25380b94bc29c75d98a203ea4ae     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  14:13:14 up  4:30,  1 user,  load average: 1.14, 0.62, 0.58

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -154,7 +154,7 @@ ssize_t ext2_data_write(device_t *dev, ext2_inode_t* inode, const void *buf, int
     return size;
 }
 
-ssize_t ext2_inode_read(device_t *dev, ext2_inode_t* inode, const void *buf, int size, int offset){
+ssize_t ext2_data_read(device_t *dev, ext2_inode_t* inode, const void *buf, int size, int offset){
     int cnt = offset/BLOCK_BYTES ,ret = 0, buf_offset = 0;
     int left;
     int first = 1;
