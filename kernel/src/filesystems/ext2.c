de3bf7627cda26ec2328a9e2617723df33d26465
tracer-nju <tracer@nju.edu.cn>
2019-06-11 11:25:09 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 4f79b25380b94bc29c75d98a203ea4ae     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  11:25:09 up  2:34,  1 user,  load average: 1.45, 1.12, 1.08

--- kernel/src/filesystems/ext2.c
+++ kernel/src/filesystems/ext2.c
@@ -193,7 +193,7 @@ int ext2_dir_search(device_t *dev, ext2_inode_t* inode, const char* name){
             dev->ops->read(dev, DATA(OFFSET_BLOCK(name_offset))+OFFSET_REMAIN(name_offset), tmp_name, cur->name_len);
 
             int clen = (name[strlen(name)-1]=='/')?strlen(name)-1:strlen(name);
-            //Log("name = %s, tmpname = %s, name_len =%d", name, tmp_name, clen);
+            Log("name = %s, tmpname = %s, name_len =%d", name, tmp_name, clen);
             if (strncmp(name, tmp_name, clen)==0){
                 finded =1;
                 break;
