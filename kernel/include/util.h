d721c38b70d508f6809146e530c253896375e7b4
tracer-nju <tracer@nju.edu.cn>
2019-06-11 12:48:14 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 4f79b25380b94bc29c75d98a203ea4ae     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  12:48:14 up  3:05,  1 user,  load average: 1.36, 0.90, 0.74

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -4,4 +4,9 @@
 int split(const char *path, char **pre, char **post);
 int split2(const char *path, char **pre, char **post);
 
+void strip(char *);
+char *rootdir(const char* path);
+char *filename(const char* path);
+char *alldir(const char *path);
+char *postname(const char *path);
 #endif
