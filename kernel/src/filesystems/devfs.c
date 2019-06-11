f8d214f48b6f02472797b8526bc9d93dd0cee3f1
tracer-nju <tracer@nju.edu.cn>
2019-06-11 23:11:53 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 639f48dbe4424d7fb88d8462db7b4d0b     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  23:11:53 up  4:27,  4 users,  load average: 0.07, 0.15, 0.07

--- kernel/src/filesystems/devfs.c
+++ kernel/src/filesystems/devfs.c
@@ -44,6 +44,8 @@ int devfs_inode_open(file_t *file, int flags){
 int devfs_inode_close(file_t *file){
     return 0;
 }
+
+#define DEV(file) ((device_t*)file->inode->fs_inode)
 const char *devfs_ls ="tty1\ntty2\ntty3\ntty4\nramdisk0\nramdisk1\n";
 ssize_t devfs_inode_read(file_t *file, char *buf, size_t size){
     ssize_t ret;
@@ -51,7 +53,7 @@ ssize_t devfs_inode_read(file_t *file, char *buf, size_t size){
         strcpy(buf, devfs_ls);
         ret = strlen(devfs_ls);
     } else {
-        ret = dev->ops->read(dev, file->offset, buf, size);
+        ret = DEV(file)->ops->read(dev, file->offset, buf, size);
     }
     return ret;
 }
@@ -60,7 +62,7 @@ ssize_t devfs_inode_write(file_t *file, const char *buf, size_t size){
     if (file->inode->id==0){
         ret = -1;
     } else {
-        ret = dev->ops->write(dev, file->offset, buf, size);
+        ret = DEV(file)->ops->write(dev, file->offset, buf, size);
     }
     return ret;
 }
