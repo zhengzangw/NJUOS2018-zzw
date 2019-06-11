cb72e2409a24ea2e6fd4b22e5a87786ad902a8ad
tracer-nju <tracer@nju.edu.cn>
2019-06-11 19:50:16 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 639f48dbe4424d7fb88d8462db7b4d0b     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  19:50:16 up  1:06,  4 users,  load average: 0.17, 0.37, 0.54

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -61,7 +61,7 @@ int vfs_unmount(const char *path){
     return finded;
 }
 
-static int get_mount(char *path){
+static int get_mount(const char *path){
     int index=-1, len = 0;
     for (int i=0;i<MAXMP;++i){
         if (mpt[i].exists && strncmp(path, mpt[i].path, strlen(mpt[i].path))==0){
