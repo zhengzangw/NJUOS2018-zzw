5b9a76848409ec2cc7ae729cf2df56ac993356f4
tracer-nju <tracer@nju.edu.cn>
2019-06-12 01:48:26 +0800

    Static hostname: zhengzangw-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 62822775e4644216a903ff6aaca701cd            Boot ID: 639f48dbe4424d7fb88d8462db7b4d0b     Virtualization: oracle   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  01:48:26 up  7:04,  4 users,  load average: 0.60, 0.46, 0.43

--- kernel/src/filesystems/procfs.c
+++ kernel/src/filesystems/procfs.c
@@ -81,7 +81,7 @@ ssize_t procfs_inode_read(file_t *file, char *buf, size_t size){
         case 1:
             break;
         case 2:
-            sprintf(buf, "Root Filesystem Info\nFS:ext2\nBlock Size:%#lx\n Inode Nums:%d\nInode Start:%d\n Inode Size:%#lx\n Data Start:%d\n",BLOCK_BYTES, ITABLE_NUM, ITABLE, sizeof(ext2_inode_t), DATA_B);
+            sprintf(buf, "Root Filesystem Info\n        FS:ext2\n Block Size:%#lx\n Inode Nums:%d\nInode Start:%d\n Inode Size:%#lx\n Data Start:%d\n",BLOCK_BYTES, ITABLE_NUM, ITABLE, sizeof(ext2_inode_t), DATA_B);
             break;
         case 3:
             task_num = atoi(file->inode->fs_inode);
